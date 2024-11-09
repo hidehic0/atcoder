c2f06f587ef1167353f912a9ebb024d125aa86bb
hidehic0 <hidehico.ooba@gmail.com>
2024-11-09 20:41:28 +0900

    複数行の入力をサポートする関数を追加

--- Training/.template/code/main.py
+++ Training/.template/code/main.py
@@ -219,20 +219,29 @@ class Heap:
 
 
 # 標準入力系
+# 一行に一つのstring
 def s():
     return sys.stdin.readline().rstrip()
 
 
+# 一行に複数のstring
 def sl():
     return s().split()
 
 
+# 一つのint
 def ii():
     return int(s())
 
 
+# 一行に複数のint
 def il(add_num: int = 0):
     return list(map(lambda i: int(i) + add_num, sl()))
+
+
+# 複数行の入力をサポート
+def li(n: int, func, *args):
+    return [func(*args) for _ in [0] * n]
 
 
 # 便利変数
