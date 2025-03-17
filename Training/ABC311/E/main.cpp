19e74a3cb4efc448c3cdea8de74b7b419641b58c
hidehic0 <hidehico.ooba@gmail.com>
2025-03-17 15:24:39 +0900

    ABC311-EをAC(定数倍で、pythonで諦めて、grokでC++に変換した)

--- Training/ABC311/E/main.cpp
+++ Training/ABC311/E/main.cpp
@@ -1,50 +1,76 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <functional>
+
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
-
-// マクロ
-
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
-
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
+// 2D array creation function
+template<typename T>
+vector<vector<T>> create_array2(int a, int b, T default_value = 0) {
+    return vector<vector<T>>(a, vector<T>(b, default_value));
 }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+// Binary search template
+int binary_search(function<bool(int)> fn, int right = 0, int left = -1, bool return_left = true) {
+    while (right - left > 1) {
+        int mid = (left + right) / 2;
+        
+        if (fn(mid)) {
+            left = mid;
+        } else {
+            right = mid;
+        }
     }
-    cout << '\n';
+    return return_left ? left : right;
 }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+// Main program
+int main() {
+    // Input
+    int H, W, N;
+    cin >> H >> W >> N;
+    
+    // Initialize 2D array
+    vector<vector<long long>> L = create_array2(H + 1, W + 1, 0LL);
+    
+    // Process N points
+    for (int _ = 0; _ < N; _++) {
+        int a, b;
+        cin >> a >> b;
+        L[a][b] += 1;
     }
-
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+    
+    // Calculate prefix sums (horizontal)
+    for (int i = 0; i <= H; i++) {
+        for (int k = 1; k <= W; k++) {
+            L[i][k] += L[i][k - 1];
+        }
+    }
+    
+    // Calculate prefix sums (vertical)
+    for (int k = 0; k <= W; k++) {
+        for (int i = 1; i <= H; i++) {
+            L[i][k] += L[i - 1][k];
+        }
+    }
+    
+    // Calculate answer
+    long long ans = 0;
+    
+    for (int i = 0; i < H; i++) {
+        for (int k = 0; k < W; k++) {
+            // Lambda function for binary search
+            auto c = [&](int mid) -> bool {
+                int u = i + mid;
+                int v = k + mid;
+                return (L[u][v] - L[i][v] - L[u][k] + L[i][k]) == 0;
+            };
+            
+            ans += binary_search(c, min(H - i, W - k) + 1, 0);
+        }
+    }
+    
+    cout << ans << endl;
+    
+    return 0;
 }
--- Training/ABC311/E/main.py
+++ Training/ABC311/E/main.py
@@ -1,346 +1,103 @@
-# ライブラリと関数と便利変数
-# ライブラリ
-from collections import deque, defaultdict, Counter
-from math import pi
-from itertools import permutations
-import bisect
 import sys
-import heapq
-from typing import List
-
-# cortedcontainersは使うときだけ wandbox非対応なので
-# from sortedcontainers import SortedDict, SortedSet, SortedList
-sys.setrecursionlimit(5 * 10**5)
+from typing import Any, Callable, List, Tuple
 
 
-# 関数
-def pow(x: int, n: int, t: int = 1):
-    # O(log N)
-    if t == 1:
-        ans = 1
-        while n:
-            if n % 2:
-                ans = ans * x
-            x = x * x
-            n >>= 1
-        return ans
-    ans = 1
-    while n:
-        if n % 2:
-            ans = (ans * x) % t
-        x = (x * x) % t
-        n >>= 1
-    return ans
+def create_array2(a: int, b: int, default: Any = 0) -> List[List[Any]]:
+    """
+    2次元配列を初期化する関数
+    """
+    return [[default] * b for _ in [0] * a]
 
 
-def is_prime(n: int) -> bool:
-    # O(√N)
-    if n == 1:
-        return False
+def binary_search(
+    fn: Callable[[int], bool], right: int = 0, left: int = -1, return_left: bool = True
+) -> int:
+    """
+    二分探索の抽象的なライブラリ
+    評価関数の結果に応じて、二分探索する
+    最終的にはleftを出力します
 
-    i = 2
-    s = n**0.5
-
-    while i < s:
-        if n % i == 0:
+    関数のテンプレート
+    def check(mid:int):
+        if A[mid] > x:
+            return True
+        else:
             return False
 
-        i += 1
+    midは必須です。それ以外はご自由にどうぞ
+    """
+    while right - left > 1:
+        mid = (left + right) // 2
 
-    return True
-
-
-def eratosthenes(n):
-    primes = [True] * (n + 1)
-    primes[0], primes[1] = False, False
-    i = 2
-    while i**2 <= n:
-        if primes[i]:
-            for k in range(i * 2, n + 1, i):
-                primes[k] = False
-
-        i += 1
-
-    return [i for i, p in enumerate(primes) if p]
-
-
-def gcd(a, b):
-    while a > 0 and b > 0:
-        if a > b:
-            a = a % b
+        if fn(mid):
+            left = mid
         else:
-            b = b % a
+            right = mid
 
-    return max(a, b)
-
-
-def lcm(a, b):
-    return (a * b) // gcd(a, b)
+    return left if return_left else right
 
 
-def calc_divisors(N):
-    # 約数全列挙
-    result = []
-
-    for i in range(1, N + 1):
-        if i * i > N:
-            break
-
-        if N % i != 0:
-            continue
-
-        heapq.heappush(result, i)
-        if N // i != i:
-            heapq.heappush(result, N // i)
-
-    return result
-
-
-def factorization(n):
-    # 素因数分解
-    result = []
-    tmp = n
-    for i in range(2, int(-(-(n**0.5) // 1)) + 1):
-        if tmp % i == 0:
-            cnt = 0
-            while tmp % i == 0:
-                cnt += 1
-                tmp //= i
-            result.append([i, cnt])
-
-    if tmp != 1:
-        result.append([tmp, 1])
-
-    if result == []:
-        result.append([n, 1])
-
-    return result
-
-
-# 標準入力系
-# 一行に一つのstring
-def s():
+def s() -> str:
+    """
+    一行に一つのstringをinput
+    """
     return sys.stdin.readline().rstrip()
 
 
-# 一行に複数のstring
-def sl():
+def sl() -> List[str]:
+    """
+    一行に複数のstringをinput
+    """
     return s().split()
 
 
-# 一つのint
-def ii():
+def ii() -> int:
+    """
+    一つのint
+    """
     return int(s())
 
 
-# 一行に複数のint
-def il(add_num: int = 0):
+def il(add_num: int = 0) -> List[int]:
+    """
+    一行に複数のint
+    """
     return list(map(lambda i: int(i) + add_num, sl()))
 
 
-# 複数行の入力をサポート
-def li(n: int, func, *args):
+def li(n: int, func, *args) -> List[List[Any]]:
+    """
+    複数行の入力をサポート
+    """
     return [func(*args) for _ in [0] * n]
 
 
-# 自作型
-class Heap:
-    def __init__(self) -> None:
-        self.heap: List[int] = []
-
-    def push(self, x: int):
-        """
-        値を挿入する関数
-
-        計算量 O(log N)
-        Nはheapのサイズ
-        """
-
-        # 末尾に挿入
-        self.heap.append(x)
-        # 現在地の変数
-        ind: int = len(self.heap) - 1
-
-        # 更新処理
-        while ind > 0:
-            # 現在地の親を求める
-            # 求め方はセグ木の要領で
-            parent: int = (ind - 1) // 2
-
-            # 親の方が小さかったら処理を終了
-            if self.heap[parent] <= x:
-                break
-
-            # 親より小さかったら入れ替える
-            self.heap[ind] = self.heap[parent]
-            # 現在地を更新
-            ind = parent
-
-        # 最後に現在地にxを代入
-        self.heap[ind] = x
-
-    def min(self):
-        """
-        最小値を出力する関数
-
-        計算量
-        O(1)
-        """
-        if len(self.heap) == 0:
-            print("\033[31m error heap is empty \033[0m")
-            return
-        return self.heap[0]
-
-    def pop(self):
-        """
-        最小値を削除する関数
-        返り値は削除した最小値
-
-        計算量
-        N = len(self.heap)
-        O(log N)
-        """
-        # 返り値を取っておく
-        result = self.min()
-
-        # self.heapが空ならエラーを吐く
-        if len(self.heap) == 0:
-            print("\033[31m error heap is empty \033[0m")
-            return
-
-        # 末尾の値を取得
-        x: int = self.heap.pop()
-        # 現在地
-        ind = 0
-
-        # 要素がなくなった場合それが最小値なので出力する
-        if len(self.heap) == 0:
-            return result
-
-        # 更新処理
-        while ind * 2 + 1 < len(self.heap):
-            # 現在地の子のインデックスを変数に入れとく
-            child1 = ind * 2 + 1
-            child2 = ind * 2 + 2
-            # child2の要素がchild1の要素より小さければchild1をchild2にする
-            if child2 < len(self.heap) and self.heap[child2] < self.heap[child1]:
-                child1 = child2
-
-            # child1の要素がxより大きければ処理を終了
-            if self.heap[child1] >= x:
-                break
-
-            # 入れ替え
-            self.heap[ind] = self.heap[child1]
-
-            # 現在地を移動
-            ind = child1
-
-        # 現在地の要素をxにする
-        self.heap[ind] = x
-
-        # 削除した値を返す
-        return result
-
-
-# 無向グラフ
-class Graph:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
-
-    def new_side(self, a: int, b: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append(b)
-        if not self.dire:
-            self.grath[b].append(a)
-
-    def side_input(self):
-        # 新しい辺をinput
-        a, b = il(-1)
-        self.new_side(a, b)
-
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
-
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
-
-
-# 有向グラフ
-class GraphW:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
-
-    def new_side(self, a: int, b: int, w: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append((b, w))
-        if not self.dire:
-            self.grath[b].append((a, w))
-
-    def side_input(self):
-        # 新しい辺をinput
-        a, b, w = il(-1)
-        self.new_side(a, b, w)
-
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
-
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
-
-
-# 便利変数
-INF = 10**18
-lowerlist = list("abcdefghijklmnopqrstuvwxyz")
-upperlist = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-
-# テンプレ
-class SegmentTree:
-    # 鉄則本のパクリですけどよろしく
-    def __init__(self, N) -> None:
-        # サイズは要素の数
-
-        self.size = 1
-        while self.size < N:
-            self.size *= 2
-
-        self.data = [0] * (self.size * 2)
-
-    def update(self, ind, x):
-        ind = ind + self.size - 1
-        self.data[ind] = x
-
-        while ind >= 2:
-            ind //= 2
-            self.data[ind] = max(self.data[ind * 2], self.data[ind * 2 + 1])
-
-    def query(self, l, r, a, b, u):
-        if r <= a or l >= b:
-            return -INF
-        if l <= a and b <= r:
-            return self.data[u]
-
-        m = (a + b) // 2
-        return max(self.query(l, r, a, m, u * 2), self.query(l, r, m, b, u * 2 + 1))
-
-
 # コード
+H, W, N = il()
+L = create_array2(H + 1, W + 1, 0)
+
+for _ in [0] * N:
+    a, b = il()
+    L[a][b] += 1
+
+for i in range(H + 1):
+    for k in range(1, W + 1):
+        L[i][k] += L[i][k - 1]
+
+for k in range(W + 1):
+    for i in range(1, H + 1):
+        L[i][k] += L[i - 1][k]
+
+ans = 0
+
+for i in range(H):
+    for k in range(W):
+
+        def c(mid):
+            u, v = i + mid, k + mid
+
+            return (L[u][v] - L[i][v] - L[u][k] + L[i][k]) == 0
+
+        ans += binary_search(c, min(H - i, W - k) + 1, 0)
+
+print(ans)
