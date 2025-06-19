19b3e603d42de0014dcbd363fc6e9e6a3913352f
hidehic0 <hidehico.ooba@gmail.com>
2025-06-19 20:48:03 +0900

    fix

--- Training/generate.py
+++ Training/generate.py
@@ -46,7 +46,7 @@ os.mkdir(path)
 problem_name_list = list("abcdefghijklmnopqrstuvwxyz")
 
 # ./templateの中身を一つずつコピー
-if problem_n >= 7:
+if problem_n >= 15:
     for i in range(problem_n):
         problem_path = f"{path}/{i + 1}"
         os.mkdir(problem_path)
