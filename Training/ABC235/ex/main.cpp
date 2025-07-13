67057c641cfe1122d423fb89eff022d011ae3484
hidehic0 <hidehico.ooba@gmail.com>
2025-07-13 11:49:04 +0900

    AC: ABC235-E

---
+++ Training/ABC235/ex/main.cpp
@@ -0,0 +1,56 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+
+// 型テンプレ
+using ll = long long;
+using ull = unsigned long long;
+
+// マクロ
+
+#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
+#define all(a) (a).begin(), (a).end()
+
+template <typename T> bool chmin(T &a, T b) {
+  if (a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+template <typename T> bool chmax(T &a, T b) {
+  if (a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+}
+
+void printbase() { cout << '\n'; }
+
+template <typename T> void printbase(const T &t) { cout << t << '\n'; }
+
+template <typename T> void printbase(const std::vector<T> &vec) {
+  for (const auto &v : vec) {
+    cout << v << ' ';
+  }
+  cout << '\n';
+}
+
+template <typename Head, typename... Tail>
+void printbase(const Head &head, const Tail &...tail) {
+  cout << head << ' ';
+  printbase(tail...);
+}
+
+#define print(...)                                                             \
+  {                                                                            \
+    printbase(__VA_ARGS__);                                                    \
+    return 0;                                                                  \
+  }
+
+const ll INF = (ll)1 << 63;
+const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+
+int main() {}
