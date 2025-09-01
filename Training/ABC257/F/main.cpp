b0cfc0eb5ec6c1c624014ed6ba5cbb0d1e22533f
hidehic0 <hidehico.ooba@gmail.com>
2025-09-01 13:52:57 +0900

    AC: ABC257-F

--- Training/ABC257/F/main.cpp
+++ Training/ABC257/F/main.cpp
@@ -1,50 +1,64 @@
+#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+int main() {
+  ll N, M, INF = pow(10, 17);
+  cin >> N >> M;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  vector G(N + 1, vi());
 
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
-
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+  rep(_, M) {
+    ll a, b;
+    cin >> a >> b;
+    G[a].emplace_back(b);
+    if (a != 0) {
+      G[b].emplace_back(a);
     }
-    cout << '\n';
-}
+  }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+  auto get_dists = [&](ll st) {
+    vi used(N + 1, INF);
+    used[st] = 0;
+    queue<ll> Q;
+    Q.push(st);
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    while (!Q.empty()) {
+      auto cur = Q.front();
+      Q.pop();
+
+      for (auto nxt : G[cur]) {
+        if (chmin(used[nxt], used[cur] + 1))
+          Q.push(nxt);
+      }
     }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+    return used;
+  };
 
-int main()
-{
+  vi dist_s = get_dists(1);
+  vi dist_g = get_dists(N);
+
+  ll p_s_cost = INF;
+  ll p_g_cost = INF;
+
+  for (auto t : G[0]) {
+    chmin(p_s_cost, dist_s[t]);
+    chmin(p_g_cost, dist_g[t]);
+  }
+
+  vi ans(N, INF);
+
+  rep(i, N) {
+    chmin(ans[i], dist_s[N]);
+    chmin(ans[i], p_s_cost + 1 + dist_g[i + 1]);
+    chmin(ans[i], p_g_cost + 1 + dist_s[i + 1]);
+    chmin(ans[i], p_g_cost + p_s_cost + 2);
+  }
+
+  rep(i, N) cout << (ans[i] == INF ? -1 : ans[i]) << (i + 1 == N ? "\n" : " ");
 }
