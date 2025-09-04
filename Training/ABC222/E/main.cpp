a1d867f3e797c3eae1c3f3d6ea13fc72b689145c
hidehic0 <hidehico.ooba@gmail.com>
2025-09-04 13:53:50 +0900

    AC: ABC222-E

--- Training/ABC222/E/main.cpp
+++ Training/ABC222/E/main.cpp
@@ -1,50 +1,98 @@
+#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+int main() {
+  ll N, M, K, T = 0;
+  cin >> N >> M >> K;
+  vi A(M);
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  rep(i, M) {
+    cin >> A[i];
+    A[i]--;
+  }
 
-void printbase() { cout << '\n'; }
+  map<pair<ll, ll>, ll> D;
+  vector<pair<ll, ll>> E(N - 1);
+  vector G(N, vector<pair<ll, ll>>());
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  rep(i, N - 1) {
+    ll u, v;
+    cin >> u >> v;
+    u--;
+    v--;
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+    if (u > v)
+      swap(u, v);
+
+    E[i] = mp(u, v);
+    G[u].push_back(mp(v, i));
+    G[v].push_back(mp(u, i));
+  }
+
+  auto dfs = [&](ll u, ll v) {
+    vi used(N, -1);
+    used[u] = u;
+
+    queue<ll> Q;
+    Q.push(u);
+
+    while (!Q.empty()) {
+      ll cur = Q.front();
+      Q.pop();
+
+      for (auto [nxt, ind] : G[cur]) {
+        if (used[nxt] == -1) {
+          used[nxt] = cur;
+          Q.push(nxt);
+        }
+      }
     }
-    cout << '\n';
-}
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+    ll cur = v;
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    while (cur != u) {
+      D[mp(min(used[cur], cur), max(used[cur], cur))]++;
+      T++;
+      cur = used[cur];
     }
+  };
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  rep(i, M - 1) { dfs(A[i], A[i + 1]); }
 
-int main()
-{
+  vector dp(T + 1, modint998244353(0));
+  dp[0] = 1;
+
+  rep(i, N - 1) {
+    ll u, v;
+    tie(u, v) = E[i];
+
+    ll cnt;
+    if (D.count(mp(u, v)))
+      cnt = D[mp(u, v)];
+    else
+      cnt = 0;
+
+    for (ll k = T; k >= 0; k--) {
+      if (k + cnt <= T) {
+        dp[k + cnt] += dp[k];
+      }
+    }
+  }
+
+  modint998244353 ans = 0;
+
+  rep(r, T + 1) {
+    ll b = T - r;
+
+    if (b <= T && r - b == K) {
+      ans += dp[r];
+    }
+  }
+
+  cout << ans.val() << "\n";
 }
