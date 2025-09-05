513f7d8fb5cea66b93094d7ca9008b9f8f98daee
hidehic0 <hidehico.ooba@gmail.com>
2025-09-05 20:17:24 +0900

    AC: ABC361-E

--- Training/ABC361/e/main.cpp
+++ Training/ABC361/e/main.cpp
@@ -1,50 +1,115 @@
+#pragma GCC TARGET("O3")
+
 #include <bits/stdc++.h>
 using namespace std;
-
-// 型テンプレ
 using ll = long long;
-using ull = unsigned long long;
 
-// マクロ
-
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
-
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
-
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+// エラトステネスの篩
+vector<int> eratosthenes(int n) {
+  vector<bool> is_prime(n + 1, true);
+  is_prime[0] = is_prime[1] = false;
+  for (int i = 2; i * 1LL * i <= n; i++) {
+    if (is_prime[i]) {
+      for (int j = i * 2; j <= n; j += i) {
+        is_prime[j] = false;
+      }
     }
-    cout << '\n';
+  }
+  vector<int> primes;
+  for (int i = 2; i <= n; i++) {
+    if (is_prime[i])
+      primes.push_back(i);
+  }
+  return primes;
 }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+// 複数数の素因数分解
+vector<vector<pair<int, int>>> factorization_plural(int nmax) {
+  vector<vector<pair<int, int>>> res(nmax + 1);
+  vector<int> primes = eratosthenes((int)sqrt(nmax) + 20);
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+  for (int n = 1; n <= nmax; n++) {
+    int tmp = n;
+    for (int p : primes) {
+      if ((ll)p * p > tmp)
+        break;
+      if (tmp % p == 0) {
+        int cnt = 0;
+        while (tmp % p == 0) {
+          tmp /= p;
+          cnt++;
+        }
+        res[n].push_back({p, cnt});
+      }
     }
+    if (tmp != 1) {
+      res[n].push_back({tmp, 1});
+    }
+  }
+  return res;
+}
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-int main()
-{
+  ll N;
+  cin >> N;
+
+  ll ans = (ll)sqrtl(N);
+
+  vector<int> L = eratosthenes(60);
+  unordered_set<int> prime_set(L.begin(), L.end());
+
+  int LIM = 1'000'001;
+  auto FL = factorization_plural(LIM);
+
+  for (int b = 3; b <= 61; b++) {
+    if (!prime_set.count(b))
+      continue;
+    if ((1LL << b) > N)
+      continue;
+
+    ll i = 2;
+    while (true) {
+      __int128 pow = 1;
+      for (int k = 0; k < b; k++) {
+        pow *= i;
+        if (pow > N)
+          break;
+      }
+      if (pow > N)
+        break;
+
+      // 素因数分解を取り出し
+      auto l = FL[i];
+      vector<pair<int, int>> factors;
+      for (auto [p, c] : l) {
+        factors.push_back({p, c * b});
+      }
+
+      bool ok = false;
+      for (int d = 2; d < b; d++) {
+        bool flag = true;
+        for (auto [p, c] : factors) {
+          if (c % d != 0) {
+            flag = false;
+            break;
+          }
+        }
+        if (flag) {
+          ok = true;
+          break;
+        }
+      }
+
+      if (!ok)
+        ans++;
+
+      i++;
+    }
+  }
+
+  cout << ans << "\n";
+  return 0;
 }
--- Training/ABC361/e/main.py
+++ Training/ABC361/e/main.py
@@ -1,6 +1,6 @@
 r"""
  ______________________
-< it's hidehico's code >
+< this is hidehic0's code >
  ----------------------
    \
     \
@@ -11,21 +11,30 @@ r"""
      (|     | )
     /'\_   _/`\
     \___)=(___/
+
+┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳
+┃                 ┳━━━━┳       ┃
+┃    私は人間です ┃ ✔  ┃       ┃
+┃                 ┻━━━━┻       ┃
+┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻
 """
 
 # ライブラリと関数と便利変数
 # ライブラリ
 import bisect
+import copy
 import heapq
+import math
 import sys
-import unittest
 from collections import Counter, defaultdict, deque
-from itertools import permutations
+from itertools import accumulate, combinations, permutations
 from math import gcd, lcm, pi
-from typing import Any, List
+from operator import itemgetter
+from typing import Any, List, Tuple
 
 # from atcoder.segtree import SegTree
 # from atcoder.lazysegtree import LazySegTree
+# from atcoder.fenwicktree import FenwickTree
 # from atcoder.dsu import DSU
 
 # cortedcontainersは使うときだけ wandbox非対応なので
@@ -35,53 +44,56 @@ from typing import Any, List
 # pypyjit.set_param("max_unroll_recursion=-1")
 
 sys.setrecursionlimit(5 * 10**5)
+import io
+import os
+import sys
+from typing import Any, List
+
+# インタラクティブ問題の時はIS_INTERACTIVEをTrueにしましょう
+# IS_INTERACTIVE = False
+
+# 標準入力関数
+# if sys.argv[0] == "Main.py":
+#     if not IS_INTERACTIVE:
+#         input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline().decode().rstrip
 
 
-# 数学型関数
-def is_prime(n):
+def s() -> str:
     """
-    素数判定します
-    計算量は定数時間です。正確には、繰り返し二乗法の計算量によりです
-    アルゴリズムはミラーラビンの素数判定を使用しています
-    nが2^64を越えると動作しません
+    一行に一つのstringをinput
     """
-    if n == 1:
-        return False
-
-    def f(a, t, n):
-        x = pow(a, t, n)
-        nt = n - 1
-        while t != nt and x != 1 and x != nt:
-            x = pow(x, 2, n)
-            t <<= 1
-
-        return t & 1 or x == nt
-
-    if n == 2:
-        return True
-    elif n % 2 == 0:
-        return False
-
-    d = n - 1
-    d >>= 1
-
-    while d & 1 == 0:
-        d >>= 1
-
-    checklist = (
-        [2, 7, 61] if 2**32 > n else [2, 325, 9375, 28178, 450775, 9780504, 1795265022]
-    )
-
-    for i in checklist:
-        if i >= n:
-            break
-        if not f(i, d, n):
-            return False
-
-    return True
+    return input()
 
 
-def eratosthenes(n):
+def sl() -> List[str]:
+    """
+    一行に複数のstringをinput
+    """
+    return s().split()
+
+
+def ii() -> int:
+    """
+    一つのint
+    """
+    return int(s())
+
+
+def il(add_num: int = 0) -> List[int]:
+    """
+    一行に複数のint
+    """
+    return list(map(lambda i: int(i) + add_num, sl()))
+
+
+def li(n: int, func, *args) -> List[List[Any]]:
+    """
+    複数行の入力をサポート
+    """
+    return [func(*args) for _ in [0] * n]
+
+
+def eratosthenes(n: int) -> List[int]:
     """
     n以下の素数を列挙します
     計算量は、O(n log log n)です
@@ -102,587 +114,71 @@ def eratosthenes(n):
     return [i for i, p in enumerate(primes) if p]
 
 
-def calc_divisors(N):
+def factorization_plural(L: List[int]) -> List[List[List[int]]]:
     """
-    Nの約数列挙します
-    計算量は、√Nです
-    約数は昇順に並んでいます
-    """
-    import heapq
-
-    result = []
-
-    for i in range(1, N + 1):
-        if i * i > N:
-            break
-
-        if N % i != 0:
-            continue
-
-        heapq.heappush(result, i)
-        if N // i != i:
-            heapq.heappush(result, N // i)
-
-    return result
-
-
-def factorization(n):
-    """
-    nを素因数分解します
-    計算量は、√Nです(要改善)
-    複数回素因数分解を行なう場合は、√N以下の素数を列挙したので試し割りした法が速いです
-    """
-    result = []
-    tmp = n
-    for i in range(2, int(-(-(n**0.5) // 1)) + 1):
-        if tmp % i == 0:
-            cnt = 0
-            while tmp % i == 0:
-                cnt += 1
-                tmp //= i
-            result.append([i, cnt])
-
-    if tmp != 1:
-        result.append([tmp, 1])
-
-    if result == []:
-        result.append([n, 1])
-
-    return result
-
-
-def simple_sigma(n: int) -> int:
-    """
-    1からnまでの総和を求める関数
-    つまり和の公式
-    """
-    return (n * (n + 1)) // 2
-
-
-# 多次元配列作成
-from typing import List, Any
-
-
-def create_array2(a: int, b: int, default: Any = 0) -> List[List[Any]]:
-    """
-    ２次元配列を初期化する関数
-    """
-    return [[default] * b for _ in [0] * a]
-
-
-def create_array3(a: int, b: int, c: int, default: Any = 0) -> List[List[List[Any]]]:
-    """
-    ３次元配列を初期化する関数
-    """
-    return [[[default] * c for _ in [0] * b] for _ in [0] * a]
-
-
-from typing import Callable
-
-
-def binary_search(fn: Callable[[int], bool], right: int = 0, left: int = -1) -> int:
-    """
-    二分探索の抽象的なライブラリ
-    評価関数の結果に応じて、二分探索する
-    最終的にはleftを出力します
-
-    関数のテンプレート
-    def check(mid:int):
-        if A[mid] > x:
-            return True
-        else:
-            return False
-
-    midは必須です。それ以外はご自由にどうぞ
-    """
-    while right - left > 1:
-        mid = (left + right) // 2
-
-        if fn(mid):
-            left = mid
-        else:
-            right = mid
-
-    return left
-
-
-def mod_add(a: int, b: int, mod: int):
-    """
-    足し算してmodを取った値を出力
-    O(1)
-    """
-    return (a + b) % mod
-
-
-def mod_sub(a: int, b: int, mod: int):
-    """
-    引き算してmodを取った値を出力
-    O(1)
-    """
-    return (a - b) % mod
-
-
-def mod_mul(a: int, b: int, mod: int):
-    """
-    掛け算してmodを取った値を出力
-    O(1)
-    """
-    return (a * b) % mod
-
-
-def mod_div(a: int, b: int, mod: int):
-    """
-    割り算してmodを取った値を出力
-    フェルマーの小定理を使って計算します
-    O(log mod)
-    """
-    return (a * pow(b, mod - 2, mod)) % mod
-
-
-class ModInt:
-    def __init__(self, x: int, mod: int = 998244353) -> None:
-        self.x = x % mod
-        self.mod = mod
-
-    def val(self):
-        return self.x
-
-    def rhs(self, rhs) -> int:
-        return rhs.x if isinstance(rhs, ModInt) else rhs
-
-    def __add__(self, rhs) -> int:
-        return mod_add(self.x, self.rhs(rhs), self.mod)
-
-    def __iadd__(self, rhs) -> "ModInt":
-        self.x = self.__add__(rhs)
-
-        return self
-
-    def __sub__(self, rhs) -> int:
-        return mod_sub(self.x, self.rhs(rhs), self.mod)
-
-    def __isub__(self, rhs) -> "ModInt":
-        self.x = self.__sub__(rhs)
-
-        return self
-
-    def __mul__(self, rhs):
-        return mod_mul(self.x, self.rhs(rhs), self.mod)
-
-    def __imul__(self, rhs):
-        self.x = self.__mul__(rhs)
-
-        return self
-
-    def __truediv__(self, rhs):
-        return mod_div(self.x, self.rhs(rhs), self.mod)
-
-    def __itruediv__(self, rhs):
-        self.x = self.__truediv__(rhs)
-
-        return self
-
-    def __floordiv__(self, rhs):
-        return (self.x // self.rhs(rhs)) % self.mod
-
-    def __ifloordiv__(self, rhs):
-        self.x = self.__floordiv__(rhs)
-
-        return self
-
-    def __pow__(self, rhs):
-        return pow(self.x, self.rhs(rhs), self.mod)
-
-    def __eq__(self, rhs) -> bool:
-        return self.rhs(rhs) == self.x
-
-    def __ne__(self, rhs) -> bool:
-        return self.rhs(rhs) != self.x
-
-
-# 標準入力関数
-import sys
-
-
-def s():
-    """
-    一行に一つのstringをinput
-    """
-    return sys.stdin.readline().rstrip()
-
-
-def sl():
-    """
-    一行に複数のstringをinput
-    """
-    return s().split()
-
-
-def ii():
-    """
-    一つのint
-    """
-    return int(s())
-
-
-def il(add_num: int = 0):
-    """
-    一行に複数のint
-    """
-    return list(map(lambda i: int(i) + add_num, sl()))
-
-
-def li(n: int, func, *args):
-    """
-    複数行の入力をサポート
-    """
-    return [func(*args) for _ in [0] * n]
-
-
-# YesNo関数
-def YesNoTemplate(state: bool, upper: bool = False) -> str:
-    """
-    stateがTrueなら、upperに応じてYes,YESをreturn
-    stateがFalseなら、upperに応じてNo,NOをreturnする
-    """
-    YES = ["Yes", "YES"]
-    NO = ["No", "NO"]
-
-    if state:
-        return YES[int(upper)]
-    else:
-        return NO[int(upper)]
-
-
-def YN(state: bool, upper: bool = False) -> None:
-    """
-    先程のYesNoTemplate関数の結果を出力する
-    """
-    res = YesNoTemplate(state, upper)
-
-    print(res)
-
-
-def YE(state: bool, upper: bool = False) -> bool | None:
-    """
-    boolがTrueならYesを出力してexit
-    """
-
-    if not state:
-        return False
-
-    YN(True, upper)
-    exit()
-
-
-def NE(state: bool, upper: bool = False) -> bool | None:
-    """
-    boolがTrueならNoを出力してexit
-    """
-
-    if not state:
-        return False
-
-    YN(False, upper)
-    exit()
-
-
-def coordinate_check(x: int, y: int, H: int, W: int) -> bool:
-    """
-    座標がグリッドの範囲内にあるかチェックする関数
-    0-indexedが前提
-    """
-
-    return 0 <= x < H and 0 <= y < W
-
-
-from typing import List, Tuple
-
-
-def grid_moves(
-    x: int,
-    y: int,
-    H: int,
-    W: int,
-    moves: List[Tuple[int]] = [(0, 1), (0, -1), (1, 0), (-1, 0)],
-    *check_funcs,
-) -> List[Tuple[int]]:
-    """
-    現在の座標から、移動可能な座標をmovesをもとに列挙します。
-    xとyは現在の座標
-    HとWはグリッドのサイズ
-    movesは移動する座標がいくつかを保存する
-    check_funcsは、その座標の点が#だとかを自前で実装して判定はこちらでするみたいな感じ
-    なおcheck_funcsは引数がxとyだけというのが条件
-    追加の判定関数は、弾く場合は、False それ以外ならTrueで
+    複数の数の素因数分解を行ないます
+    計算量は、O(N * (√max(L) log log √max(L)))
+    みたいな感じです
+
+    最初に素数を列挙するため、普通の素因数分解より効率がいいです
     """
     res = []
+    primes = eratosthenes(int(max(L) ** 0.5) + 20)
 
-    for mx, my in moves:
-        nx, ny = x + mx, y + my
+    def solve(n):
+        t = []
+        for p in primes:
+            if n % p == 0:
+                cnt = 0
+                while n % p == 0:
+                    cnt += 1
+                    n //= p
 
-        if not coordinate_check(nx, ny, H, W):
-            continue
+                t.append([p, cnt])
 
-        for f in check_funcs:
-            if not f(nx, ny):
-                break
-        else:
-            res.append((nx, ny))
+        if n != 1:
+            t.append([n, 1])
+
+        if t == []:
+            t.append([n, 1])
+
+        return t
+
+    for n in L:
+        res.append(solve(n))
 
     return res
 
 
-# ac_libraryのメモ
-"""
-segtree
-
-初期化するとき
-Segtree(op,e,v)
-
-opはマージする関数
-例
-
-def op(a,b):
-    return a+b
-
-eは初期化する値
-
-vは配列の長さまたは、初期化する内容
-"""
-# グラフ構造
-# 無向グラフ
-from collections import deque
-from typing import List
-
-
-class Graph:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
-        self.in_deg = [0] * N
-
-    def new_side(self, a: int, b: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append(b)
-        if self.dire:
-            self.in_deg[b] += 1
-
-        if not self.dire:
-            self.grath[b].append(a)
-
-    def side_input(self):
-        # 新しい辺をinput
-        a, b = map(lambda x: int(x) - 1, input().split())
-        self.new_side(a, b)
-
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
-
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
-
-    def topological(self, unique: bool = False):
-        if not self.dire:
-            raise ValueError("グラフが有向グラフでは有りません (╥﹏╥)")
-
-        in_deg = self.in_deg[:]
-
-        S: deque[int] = deque([])
-        order: List[int] = []
-
-        for i in range(self.N):
-            if in_deg[i] == 0:
-                S.append(i)
-
-        while S:
-            if unique and len(S) != 1:
-                return [-1]
-
-            cur = S.pop()
-            order.append(cur)
-
-            for nxt in self.get(cur):
-                in_deg[nxt] -= 1
-
-                if in_deg[nxt] == 0:
-                    S.append(nxt)
-
-        if sum(in_deg) > 0:
-            return [-1]
-        else:
-            return [x for x in order]
-
-
-# 重み付きグラフ
-class GraphW:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
-
-    def new_side(self, a: int, b: int, w: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append((b, w))
-        if not self.dire:
-            self.grath[b].append((a, w))
-
-    def side_input(self):
-        # 新しい辺をinput
-        a, b, w = map(lambda x: int(x) - 1, input().split())
-        self.new_side(a, b, w + 1)
-
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
-
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
-
-
-# UnionFind木
-class UnionFind:
-    """
-    rollbackをデフォルトで装備済み
-    計算量は、経路圧縮を行わないため、基本的なUnionFindの動作は、一回あたり、O(log N)
-    rollbackは、一回あたり、O(1)で行える。
-    """
-
-    def __init__(self, n: int) -> None:
-        self.size = n
-        self.data = [-1] * n
-        self.hist = []
-
-    def root(self, vtx: int) -> int:
-        if self.data[vtx] < 0:
-            return vtx
-
-        return self.root(self.data[vtx])
-
-    def same(self, a: int, b: int):
-        return self.root(a) == self.root(b)
-
-    def unite(self, a: int, b: int) -> bool:
-        """
-        rootが同じでも、履歴には追加する
-        """
-        ra, rb = self.root(a), self.root(b)
-
-        # 履歴を作成する
-        new_hist = [ra, rb, self.data[ra], self.data[rb]]
-        self.hist.append(new_hist)
-
-        if ra == rb:
-            return False
-
-        if self.data[ra] > self.data[rb]:
-            ra, rb = rb, ra
-
-        self.data[ra] += self.data[rb]
-        self.data[rb] = ra
-
-        return True
-
-    def rollback(self):
-        """
-        undoします
-        redoはありません
-        """
-        if not self.hist:
-            return False
-
-        ra, rb, da, db = self.hist.pop()
-        self.data[ra] = da
-        self.data[rb] = db
-        return True
-
-
-# Trie木
-class Trie:
-    class Data:
-        def __init__(self, value, ind):
-            self.count = 1
-            self.value = value
-            self.childs = {}
-            self.ind = ind
-
-    def __init__(self):
-        self.data = [self.Data("ab", 0)]  # 初期値はabにして被らないようにする
-
-    def add(self, value: str) -> int:
-        cur = 0
-        result = 0
-
-        # 再帰的に探索する
-        for t in value:
-            childs = self.data[cur].childs  # 参照渡しで
-
-            if t in childs:
-                self.data[childs[t]].count += 1
-            else:
-                nd = self.Data(t, len(self.data))
-                childs[t] = len(self.data)
-                self.data.append(nd)
-
-            result += self.data[childs[t]].count - 1
-            cur = childs[t]
-
-        return result
-
-    def lcp_max(self, value: str) -> int:
-        cur = 0
-        result = 0
-
-        for t in value:
-            childs = self.data[cur].childs
-
-            if t not in childs:
-                break
-
-            if self.data[childs[t]].count == 1:
-                break
-
-            cur = childs[t]
-            result += 1
-
-        return result
-
-    def lcp_sum(self, value: str) -> int:
-        cur = 0
-        result = 0
-
-        for t in value:
-            childs = self.data[cur].childs
-
-            if t not in childs:
-                break
-
-            if self.data[childs[t]].count == 1:
-                break
-
-            cur = childs[t]
-            result += self.data[childs[t]].count - 1
-
-        return result
-
-
-# 便利変数
-INF = 1 << 63
-lowerlist = list("abcdefghijklmnopqrstuvwxyz")
-upperlist = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
 # コード
+N = int(input())
+
+ans = math.isqrt(N)
+
+L = eratosthenes(60)
+FL = factorization_plural(range(1, 10**6 + 2))
+
+for b in range(3, 62):
+    if b not in L or 2**b > N:
+        continue
+
+    i = 2
+
+    while i**b <= N:
+        l = FL[i - 1]
+        l = [[t[0], t[1] * b] for t in l]
+
+        for i in range(2, b):
+            flag = True
+            for t in l:
+                if t[1] % i != 0:
+                    flag = False
+                    break
+
+            if flag:
+                break
+        else:
+            ans += 1
+            i += 1
+
+print(ans)
