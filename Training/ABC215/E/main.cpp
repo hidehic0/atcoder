36622da737c38d616c06d0cc28e374c7af322703
hidehic0 <hidehico.ooba@gmail.com>
2025-09-10 11:16:33 +0900

    AC: ABC215-F

--- Training/ABC215/E/main.cpp
+++ Training/ABC215/E/main.cpp
@@ -1,50 +1,87 @@
+#include <atcoder/fenwicktree>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace atcoder;
 
-// 型テンプレ
 using ll = long long;
-using ull = unsigned long long;
+using pii = pair<int, int>;
 
-// マクロ
+// 座標圧縮
+template <class T> auto compress_1d(const vector<T> &points) {
+  vector<T> vals(points.begin(), points.end());
+  sort(vals.begin(), vals.end());
+  vals.erase(unique(vals.begin(), vals.end()), vals.end());
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
-
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
+  unordered_map<T, int> d;
+  for (int i = 0; i < (int)vals.size(); i++) {
+    d[vals[i]] = i;
+  }
+  return d;
 }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+// 二分探索
+template <class F>
+int binary_search(F fn, int right, int left = -1, bool return_left = true) {
+  while (right - left > 1) {
+    int mid = (left + right) / 2;
+    if (fn(mid))
+      left = mid;
+    else
+      right = mid;
+  }
+  return return_left ? left : right;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int N;
+  cin >> N;
+  vector<pii> L(N);
+  for (int i = 0; i < N; i++) {
+    int a, b;
+    cin >> a >> b;
+    L[i] = {a, b};
+  }
+
+  sort(L.begin(), L.end());
+  vector<int> Y;
+  for (auto &p : L)
+    Y.push_back(p.second);
+  sort(Y.begin(), Y.end());
+
+  auto y_map = compress_1d(Y);
+
+  auto f = [&](int k) -> bool {
+    fenwick_tree<int> ft(N);
+    int cur = 0;
+    for (int i = 1; i < N; i++) {
+      while (cur < i && L[i].first - L[cur].first >= k) {
+        ft.add(y_map[L[cur].second], 1);
+        cur++;
+      }
+
+      int a = -1, b = -1;
+      int bis_a = lower_bound(Y.begin(), Y.end(), L[i].second + k) - Y.begin();
+      if (bis_a < N) {
+        a = ft.sum(y_map[Y[bis_a]], N);
+      }
+
+      int bis_b =
+          (int)(upper_bound(Y.begin(), Y.end(), L[i].second - k) - Y.begin()) -
+          1;
+      if (bis_b >= 0 && abs(L[i].second - Y[bis_b]) >= k) {
+        b = ft.sum(0, y_map[Y[bis_b]] + 1);
+      }
+
+      if ((a >= 1) || (b >= 1))
+        return true;
     }
-    cout << '\n';
-}
+    return false;
+  };
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
-
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+  int ans = binary_search(
+      f, min(Y.back() - Y.front(), L.back().first - L.front().first) + 1, 0);
+  cout << ans << "\n";
 }
--- Training/ABC215/E/main.py
+++ Training/ABC215/E/main.py
@@ -1,399 +1,185 @@
+r"""
+ ______________________
+< this is hidehic0's code >
+ ----------------------
+   \
+    \
+        .--.
+       |o_o |
+       |:_/ |
+      //   \ \
+     (|     | )
+    /'\_   _/`\
+    \___)=(___/
+
+┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┳
+┃                 ┳━━━━┳       ┃
+┃    私は人間です ┃ ✔  ┃       ┃
+┃                 ┻━━━━┻       ┃
+┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┻
+"""
+
 # ライブラリと関数と便利変数
 # ライブラリ
-from collections import deque, defaultdict, Counter
-from math import pi
-from itertools import permutations
 import bisect
-import sys
+import copy
 import heapq
-from typing import List
+import math
+import sys
+from collections import Counter, defaultdict, deque
+from itertools import accumulate, combinations, permutations
+from math import gcd, lcm, pi
+from operator import itemgetter
+from typing import Any, List, Tuple
+
+# from atcoder.segtree import SegTree
+# from atcoder.lazysegtree import LazySegTree
+from atcoder.fenwicktree import FenwickTree
+
+# from atcoder.dsu import DSU
 
 # cortedcontainersは使うときだけ wandbox非対応なので
 # from sortedcontainers import SortedDict, SortedSet, SortedList
 
+# import pypyjit
+# pypyjit.set_param("max_unroll_recursion=-1")
+
 sys.setrecursionlimit(5 * 10**5)
+import io
+import os
+import sys
+from typing import Any, List
+
+# インタラクティブ問題の時はIS_INTERACTIVEをTrueにしましょう
+# IS_INTERACTIVE = False
+
+# 標準入力関数
+# if sys.argv[0] == "Main.py":
+#     if not IS_INTERACTIVE:
+#         input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline().decode().rstrip
 
 
-# 関数
-def pow(x: int, n: int, t: int = 1):
-    # O(log N)
-    if t == 1:
-        ans = 1
-        while n:
-            if n % 2:
-                ans = ans * x
-            x = x * x
-            n >>= 1
-        return ans
-    ans = 1
-    while n:
-        if n % 2:
-            ans = (ans * x) % t
-        x = (x * x) % t
-        n >>= 1
-    return ans
+def s() -> str:
+    """
+    一行に一つのstringをinput
+    """
+    return input()
 
 
-def is_prime(n: int) -> bool:
-    # O(√N)
-    if n == 1:
-        return False
-
-    i = 2
-    s = n**0.5
-
-    while i < s:
-        if n % i == 0:
-            return False
-
-        i += 1
-
-    return True
-
-
-def eratosthenes(n):
-    primes = [True] * (n + 1)
-    primes[0], primes[1] = False, False
-    i = 2
-    while i**2 <= n:
-        if primes[i]:
-            for k in range(i * 2, n + 1, i):
-                primes[k] = False
-
-        i += 1
-
-    return [i for i, p in enumerate(primes) if p]
-
-
-def gcd(a, b):
-    while a > 0 and b > 0:
-        if a > b:
-            a = a % b
-        else:
-            b = b % a
-
-    return max(a, b)
-
-
-def lcm(a, b):
-    return (a * b) // gcd(a, b)
-
-
-def calc_divisors(N):
-    # 約数全列挙
-    result = []
-
-    for i in range(1, N + 1):
-        if i * i > N:
-            break
-
-        if N % i != 0:
-            continue
-
-        heapq.heappush(result, i)
-        if N // i != i:
-            heapq.heappush(result, N // i)
-
-    return result
-
-
-def factorization(n):
-    # 素因数分解
-    result = []
-    tmp = n
-    for i in range(2, int(-(-(n**0.5) // 1)) + 1):
-        if tmp % i == 0:
-            cnt = 0
-            while tmp % i == 0:
-                cnt += 1
-                tmp //= i
-            result.append([i, cnt])
-
-    if tmp != 1:
-        result.append([tmp, 1])
-
-    if result == []:
-        result.append([n, 1])
-
-    return result
-
-
-# 標準入力系
-# 一行に一つのstring
-def s():
-    return sys.stdin.readline().rstrip()
-
-
-# 一行に複数のstring
-def sl():
+def sl() -> List[str]:
+    """
+    一行に複数のstringをinput
+    """
     return s().split()
 
 
-# 一つのint
-def ii():
+def ii() -> int:
+    """
+    一つのint
+    """
     return int(s())
 
 
-# 一行に複数のint
-def il(add_num: int = 0):
+def il(add_num: int = 0) -> List[int]:
+    """
+    一行に複数のint
+    """
     return list(map(lambda i: int(i) + add_num, sl()))
 
 
-# 複数行の入力をサポート
-def li(n: int, func, *args):
+def li(n: int, func, *args) -> List[List[Any]]:
+    """
+    複数行の入力をサポート
+    """
     return [func(*args) for _ in [0] * n]
 
 
-# 自作型
-class Heap:
-    def __init__(self) -> None:
-        self.heap: List[int] = []
-
-    def push(self, x: int):
-        """
-        値を挿入する関数
-
-        計算量 O(log N)
-        Nはheapのサイズ
-        """
-
-        # 末尾に挿入
-        self.heap.append(x)
-        # 現在地の変数
-        ind: int = len(self.heap) - 1
-
-        # 更新処理
-        while ind > 0:
-            # 現在地の親を求める
-            # 求め方はセグ木の要領で
-            parent: int = (ind - 1) // 2
-
-            # 親の方が小さかったら処理を終了
-            if self.heap[parent] <= x:
-                break
-
-            # 親より小さかったら入れ替える
-            self.heap[ind] = self.heap[parent]
-            # 現在地を更新
-            ind = parent
-
-        # 最後に現在地にxを代入
-        self.heap[ind] = x
-
-    def min(self):
-        """
-        最小値を出力する関数
-
-        計算量
-        O(1)
-        """
-        if len(self.heap) == 0:
-            print("\033[31m error heap is empty \033[0m")
-            return
-        return self.heap[0]
-
-    def pop(self):
-        """
-        最小値を削除する関数
-        返り値は削除した最小値
-
-        計算量
-        N = len(self.heap)
-        O(log N)
-        """
-        # 返り値を取っておく
-        result = self.min()
-
-        # self.heapが空ならエラーを吐く
-        if len(self.heap) == 0:
-            print("\033[31m error heap is empty \033[0m")
-            return
-
-        # 末尾の値を取得
-        x: int = self.heap.pop()
-        # 現在地
-        ind = 0
-
-        # 要素がなくなった場合それが最小値なので出力する
-        if len(self.heap) == 0:
-            return result
-
-        # 更新処理
-        while ind * 2 + 1 < len(self.heap):
-            # 現在地の子のインデックスを変数に入れとく
-            child1 = ind * 2 + 1
-            child2 = ind * 2 + 2
-            # child2の要素がchild1の要素より小さければchild1をchild2にする
-            if child2 < len(self.heap) and self.heap[child2] < self.heap[child1]:
-                child1 = child2
-
-            # child1の要素がxより大きければ処理を終了
-            if self.heap[child1] >= x:
-                break
-
-            # 入れ替え
-            self.heap[ind] = self.heap[child1]
-
-            # 現在地を移動
-            ind = child1
-
-        # 現在地の要素をxにする
-        self.heap[ind] = x
-
-        # 削除した値を返す
-        return result
+from typing import Callable
 
 
-# 無向グラフ
-class Graph:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
+def binary_search(
+    fn: Callable[[int], bool], right: int = 0, left: int = -1, return_left: bool = True
+) -> int:
+    """
+    二分探索の抽象的なライブラリ
+    評価関数の結果に応じて、二分探索する
+    最終的にはleftを出力します
 
-    def new_side(self, a: int, b: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append(b)
-        if not self.dire:
-            self.grath[b].append(a)
+    関数のテンプレート
+    def check(mid:int):
+        if A[mid] > x:
+            return True
+        else:
+            return False
 
-    def side_input(self):
-        # 新しい辺をinput
-        a, b = il(-1)
-        self.new_side(a, b)
+    midは必須です。それ以外はご自由にどうぞ
+    """
+    while right - left > 1:
+        mid = (left + right) // 2
 
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
+        if fn(mid):
+            left = mid
+        else:
+            right = mid
 
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
+    return left if return_left else right
 
 
-# 有向グラフ
-class GraphW:
-    def __init__(self, N: int, dire: bool = False) -> None:
-        self.N = N
-        self.dire = dire
-        self.grath = [[] for _ in [0] * self.N]
+def compress_1d(points: List[int] | Tuple[int]) -> List[int]:
+    """
+    一次元座標圧縮します
+    計算量は、O(N log N)です
 
-    def new_side(self, a: int, b: int, w: int):
-        # 注意　0-indexedが前提
-        self.grath[a].append((b, w))
-        if not self.dire:
-            self.grath[b].append((a, w))
+    lとrは、まとめて入れる事で、座圧できます
+    """
+    res = []
+    d = {num: ind for ind, num in enumerate(sorted(set(points)))}
 
-    def side_input(self):
-        # 新しい辺をinput
-        a, b, w = il(-1)
-        self.new_side(a, b, w)
+    for a in points:
+        res.append(d[a])
 
-    def input(self, M: int):
-        # 複数行の辺のinput
-        for _ in [0] * M:
-            self.side_input()
-
-    def get(self, a: int):
-        # 頂点aの隣接点を出力
-        return self.grath[a]
-
-    def all(self):
-        # グラフの内容をすべて出力
-        return self.grath
-
-
-# 便利変数
-INF = 10**18
-lowerlist = list("abcdefghijklmnopqrstuvwxyz")
-upperlist = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
-
-# テンプレ
-class RMAQ:
-    def __init__(self, N) -> None:
-        # サイズは要素の数
-
-        self.size = 1
-        while self.size < N:
-            self.size *= 2
-
-        self.data = [0] * (self.size * 2)
-
-    def update(self, ind, x):
-        ind = ind + self.size - 1
-        self.data[ind] = x
-
-        while ind >= 2:
-            ind //= 2
-            self.data[ind] = max(self.data[ind * 2], self.data[ind * 2 + 1])
-
-    def query(self, l: int, r: int, a: int, b: int, u: int):
-        if r <= a or l >= b:
-            return -INF
-        if l <= a and b <= r:
-            return self.data[u]
-
-        m = (a + b) // 2
-        return max(self.query(l, r, a, m, u * 2), self.query(l, r, m, b, u * 2 + 1))
-
-
-class RMIQ:
-    def __init__(self, N) -> None:
-        # サイズは要素の数
-
-        self.size = 1
-        while self.size < N:
-            self.size *= 2
-
-        self.data = [0] * (self.size * 2)
-
-    def update(self, ind, x):
-        ind = ind + self.size - 1
-        self.data[ind] = x
-
-        while ind >= 2:
-            ind //= 2
-            self.data[ind] = min(self.data[ind * 2], self.data[ind * 2 + 1])
-
-    def query(self, l: int, r: int, a: int, b: int, u: int):
-        if r <= a or l >= b:
-            return INF
-        if l <= a and b <= r:
-            return self.data[u]
-
-        m = (a + b) // 2
-        return min(self.query(l, r, a, m, u * 2), self.query(l, r, m, b, u * 2 + 1))
-
-
-class RSQ:
-    def __init__(self, n) -> None:
-        self.size = 1
-        while self.size < n:
-            self.size *= 2
-
-        self.data = [0] * (self.size * 2)
-
-    def update(self, ind, x):
-        ind += self.size
-        self.data[ind] = x
-        while ind >= 2:
-            ind //= 2
-            self.data[ind] = self.data[ind * 2] + self.data[ind * 2 + 1]
-
-    def query(self, l, r, a, b, u):
-        if r <= a or b <= l:
-            return 0
-        if l <= a and b <= r:
-            return self.data[u]
-        m = (a + b) // 2
-
-        return self.query(l, r, a, m, u * 2) + self.query(l, r, m, b, u * 2 + 1)
+    return d
 
 
 # コード
+N = ii()
+L = li(N, il)
+L.sort(key=lambda x: x[0])
+Y = [x[1] for x in L]
+Y.sort()
+
+y_map = compress_1d([x[1] for x in L])
+
+
+def f(k):
+    ft = FenwickTree(N)
+
+    cur = 0
+
+    for i in range(1, N):
+        while cur < i and L[i][0] - L[cur][0] >= k:
+            ft.add(y_map[L[cur][1]], 1)
+            cur += 1
+
+        bis_a = bisect.bisect_left(Y, L[i][1] + k)
+        a = None
+        if bis_a >= N:
+            a = -1
+        else:
+            a = ft.sum(y_map[Y[bis_a]], N)
+
+        bis_b = bisect.bisect_right(Y, L[i][1] - k) - 1
+        b = None
+
+        if abs(L[i][1] - Y[bis_b]) >= k:
+            b = ft.sum(0, y_map[Y[bis_b]] + 1)
+        else:
+            b = -1
+
+        if b >= 1 or a >= 1:
+            return True
+
+    return False
+
+
+print(binary_search(f, min(Y[-1] - Y[0], L[-1][0] - L[0][0]) + 1, 0))
