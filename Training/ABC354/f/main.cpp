94b67425a81fec67caf951509c748c554b8ebb48
hidehic0 <hidehico.ooba@gmail.com>
2025-09-11 20:57:49 +0900

    C: ABC354-F

--- Training/ABC354/f/main.cpp
+++ Training/ABC354/f/main.cpp
@@ -1,50 +1,77 @@
+#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+template <typename T> map<T, T> compress(vector<T> L) {
+  set<T> st;
+  map<T, T> res;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  rep(i, L.size()) st.emplace(L[i]);
 
-void printbase() { cout << '\n'; }
+  auto it = st.begin();
+  T i = 1;
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
+  while (it != st.end()) {
+    T tmp = *it;
+    res.emplace(tmp, i);
+    i++;
+    it++;
+  }
+
+  return res;
 }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+ll op(ll a, ll b) { return max(a, b); }
+ll e() { return 0; }
+
+int main() {
+  ll T;
+  cin >> T;
+  while (T--) {
+    ll N;
+    cin >> N;
+    vi A(N);
+    rep(i, N) cin >> A[i];
+
+    auto D = compress(A);
+
+    segtree<ll, op, e> seg_fe(N + 1);
+    segtree<ll, op, e> seg_fb(N + 1);
+    vi prev;
+
+    rrep(i, N) {
+      prev.emplace_back(seg_fe.get(D[A[i]]));
+      if (D[A[i]] == N) {
+        seg_fe.set(D[A[i]], 1);
+        continue;
+      }
+
+      seg_fe.set(D[A[i]], seg_fe.prod(D[A[i]] + 1, N + 1) + 1);
     }
-    cout << '\n';
-}
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+    reverse(all(prev));
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    ll lis_l = seg_fe.all_prod();
+    vi ans;
+
+    rep(i, N) {
+      seg_fe.set(D[A[i]], prev[i]);
+      ll l = seg_fb.prod(0, D[A[i]]) + 1 +
+             (D[A[i]] == N ? 0 : seg_fe.prod(D[A[i]] + 1, N + 1));
+
+      if (l == lis_l) {
+        ans.emplace_back(i + 1);
+      }
+
+      seg_fb.set(D[A[i]], seg_fb.prod(0, D[A[i]]) + 1);
     }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+    cout << ans.size() << "\n";
 
-int main()
-{
+    rep(i, ans.size()) cout << ans[i] << (i + 1 == ans.size() ? "\n" : " ");
+  }
 }
