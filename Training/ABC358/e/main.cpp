89d73934d1f891f370bb1ca54dcfd42038d67c48
hidehic0 <hidehico.ooba@gmail.com>
2025-09-26 10:07:32 +0900

    AC: ABC358-E

--- Training/ABC358/e/main.cpp
+++ Training/ABC358/e/main.cpp
@@ -6,4 +6,36 @@ using namespace atcoder;
 #include "templates/alias.hpp"
 #include "templates/macro.hpp"
 
-int main() {}
+int main() {
+  ll K, N = 26;
+  input(K);
+  vi C(N);
+  input(C);
+
+  vector<modint998244353> F = {modint998244353{1}, modint998244353{1}}, FINV;
+
+  REP(i, 2, 1003) { F.emplace_back(*prev(F.end()) * i); }
+  rep(i, F.size()) { FINV.emplace_back(F[i].inv()); }
+
+  vector dp(N + 1, vector(K + 1, modint998244353{0}));
+  dp[0][0] += 1;
+
+  rep(i, N) {
+    rep(k, K + 1) {
+      dp[i + 1][k] += dp[i][k];
+
+      REP(c, 1, C[i] + 1) {
+        if (k + c <= K) {
+          modint998244353 b = F[k + c] * FINV[c] * FINV[k];
+          dp[i + 1][k + c] += dp[i][k] * b;
+        }
+      }
+    }
+  }
+
+  modint998244353 ans = -1;
+
+  rep(i, K + 1) { ans += dp[N][i]; }
+
+  cout << ans.val() << "\n";
+}
