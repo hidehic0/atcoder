db9137228d3d082a0dc9d114b8c4bcf3f344773e
hidehic0 <hidehico.ooba@gmail.com>
2025-10-06 11:39:34 +0900

    AC: ABC344-F

--- Training/ABC344/F/main.cpp
+++ Training/ABC344/F/main.cpp
@@ -1,50 +1,53 @@
+#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+int main() {
+  ll N, INF = 5e17;
+  cin >> N;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  vector P(N, vi(N)), R(N, vi(N - 1)), D(N - 1, vi(N));
+  vector<vector<pii>> dp(N, vector<pii>(N, {INF, INF}));
+  dp[0][0] = {0, 0};
 
-void printbase() { cout << '\n'; }
+  rep(i, N) rep(k, N) cin >> P[i][k];
+  rep(i, N) rep(k, N - 1) cin >> R[i][k];
+  rep(i, N - 1) rep(k, N) cin >> D[i][k];
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  rep(i, N) {
+    rep(k, N) {
+      ll cnt, m;
+      tie(cnt, m) = dp[i][k];
+      m *= -1;
+      vvi dis(N, vi(N, INF));
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+      dis[i][k] = 0;
+
+      REP(nx, i, N) {
+        REP(ny, k, N) {
+          if (nx == i && ny == k)
+            continue;
+
+          if (nx - 1 >= 0) {
+            chmin(dis[nx][ny], dis[nx - 1][ny] + D[nx - 1][ny]);
+          }
+
+          if (ny - 1 >= 0) {
+            chmin(dis[nx][ny], dis[nx][ny - 1] + R[nx][ny - 1]);
+          }
+
+          ll pc = max(dis[nx][ny] - m + P[i][k] - 1, 0LL) / P[i][k];
+          ll nm = (pc * P[i][k]) + m - dis[nx][ny];
+
+          chmin(dp[nx][ny], mp(pc + cnt + nx - i + ny - k, -nm));
+        }
+      }
     }
-    cout << '\n';
-}
+  }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
-
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+  cout << dp[N - 1][N - 1].first << "\n";
 }
