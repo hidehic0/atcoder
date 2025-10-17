26d631913a543695c29bef60418dd2d1c9f5d12a
hidehic0 <hidehico.ooba@gmail.com>
2025-10-17 15:46:00 +0900

    AC: ABC267-E

--- Training/ABC267/E/main.cpp
+++ Training/ABC267/E/main.cpp
@@ -1,50 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
+ll N, M;
+vi A;
+vvi G;
 
-// マクロ
+bool check(ll x) {
+  vi cos(N, 0);
+  vector<bool> used(N, false);
+  vector<pii> PQ;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  rep(u, N) {
+    for (ll v : G[u])
+      cos[u] += A[v];
 
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
-
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+    if (cos[u] <= x) {
+      used[u] = true;
+      PQ.emplace_back(cos[u], u);
     }
-    cout << '\n';
-}
+  }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+  while (!PQ.empty()) {
+    ll c, u;
+    tie(c, u) = PQ.back();
+    PQ.pop_back();
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    for (ll v : G[u]) {
+      if (!used[v]) {
+        cos[v] -= A[u];
+        if (cos[v] <= x) {
+          used[v] = true;
+          PQ.emplace_back(cos[v], v);
+        }
+      }
     }
+  }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  return count(all(used), true) == N;
+}
 
-int main()
-{
+int main() {
+  cin >> N >> M;
+  A.resize(N);
+
+  rep(i, N) cin >> A[i];
+  G.resize(N);
+
+  rep(_, M) {
+    ll u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+
+    G[u].emplace_back(v);
+    G[v].emplace_back(u);
+  }
+
+  ll left = -1, right = 3e14;
+
+  while (right - left > 1) {
+    ll mid = (left + right) / 2;
+
+    if (check(mid)) {
+      right = mid;
+    } else {
+      left = mid;
+    }
+  }
+
+  cout << left + 1 << "\n";
 }
