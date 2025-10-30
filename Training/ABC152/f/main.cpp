78e473a4b2a281027f15a38d144ba46b34ef09c7
hidehic0 <hidehico.ooba@gmail.com>
2025-10-30 12:34:56 +0900

    AC: ABC152-F

--- Training/ABC152/f/main.cpp
+++ Training/ABC152/f/main.cpp
@@ -1,56 +1,98 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+using bs = bitset<60>;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+int main() {
+  ll N, M;
+  cin >> N;
+  vvi G(N);
+  vvi T(N, vi(N, -1));
 
-template <typename T> bool chmin(T &a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-template <typename T> bool chmax(T &a, T b) {
-  if (a < b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
+  rep(i, N - 1) {
+    ll a, b;
+    cin >> a >> b;
+    a--;
+    b--;
 
-void printbase() { cout << '\n'; }
+    G[a].emplace_back(b);
+    G[b].emplace_back(a);
 
-template <typename T> void printbase(const T &t) { cout << t << '\n'; }
-
-template <typename T> void printbase(const std::vector<T> &vec) {
-  for (const auto &v : vec) {
-    cout << v << ' ';
-  }
-  cout << '\n';
-}
-
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail) {
-  cout << head << ' ';
-  printbase(tail...);
-}
-
-#define print(...)                                                             \
-  {                                                                            \
-    printbase(__VA_ARGS__);                                                    \
-    return 0;                                                                  \
+    T[a][b] = i;
+    T[b][a] = i;
   }
 
-const ll INF = (ll)1 << 63;
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  auto getpath = [&](ll a, ll b) {
+    queue<ll> Q;
+    Q.emplace(a);
 
-int main() {}
+    vi prev(N, -1);
+    prev[a] = -2;
+    bs res{};
+
+    while (!Q.empty()) {
+      ll cur = Q.front();
+      Q.pop();
+
+      for (ll nxt : G[cur]) {
+        if (prev[nxt] == -1) {
+          prev[nxt] = cur;
+          Q.emplace(nxt);
+
+          if (nxt == b) {
+            break;
+          }
+        }
+      }
+    }
+
+    ll cur = b;
+
+    while (prev[cur] != -2) {
+      res[T[cur][prev[cur]]] = 1;
+      cur = prev[cur];
+    }
+
+    return res;
+  };
+
+  cin >> M;
+  VC<bs> P(M);
+
+  bs cur{};
+
+  rep(i, M) {
+    ll u, v;
+    cin >> u >> v;
+    u--;
+    v--;
+    P[i] = getpath(u, v);
+
+    cur |= P[i];
+  }
+
+  ll ans = 0;
+
+  REP(bit, 0, 1 << M) {
+    bs tmp;
+    ll cnt = 0;
+
+    rep(i, M) {
+      if (bit & (1 << i)) {
+        tmp |= P[i];
+        cnt++;
+      }
+    }
+
+    if (cnt % 2 == 0) {
+      ans += (1LL << ((N - 1) - tmp.count()));
+    } else {
+      ans -= (1LL << ((N - 1) - tmp.count()));
+    }
+  }
+
+  cout << ans << "\n";
+}
