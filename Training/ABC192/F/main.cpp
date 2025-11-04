933924fbcf8a0f35722cabbd633ac6a41b3b4968
hidehic0 <hidehico.ooba@gmail.com>
2025-11-04 21:06:07 +0900

    AC: ABC192-F

--- Training/ABC192/F/main.cpp
+++ Training/ABC192/F/main.cpp
@@ -1,50 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+int main() {
+  ll N, X;
+  cin >> N >> X;
+  vi A(N);
+  input(A);
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  ll ans = LONG_LONG_MAX;
 
-void printbase() { cout << '\n'; }
+  REP(n, 1, N + 1) {
+    vvi dp(n + 1, vi(n, -1e18));
+    dp[0][X % n] = 0;
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
-
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
-    }
-    cout << '\n';
-}
-
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    rep(i, N) {
+      rrep(k, n) {
+        rep(m, n) {
+          chmax(dp[k + 1][((m - A[i]) % n + n) % n], dp[k][m] + A[i]);
+        }
+      }
     }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+    if (dp[n][0] >= 0) {
+      chmin(ans, (X - dp[n][0]) / n);
+    }
+  }
 
-int main()
-{
+  cout << ans << "\n";
 }
