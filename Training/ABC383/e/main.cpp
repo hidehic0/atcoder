1acb06f0a413e1beab65ccd557931587c335e9d5
hidehic0 <hidehico.ooba@gmail.com>
2025-11-17 12:49:21 +0900

    AC: ABC383-E

--- Training/ABC383/e/main.cpp
+++ Training/ABC383/e/main.cpp
@@ -1,9 +1,60 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
-using namespace atcoder;
 
+#include "data-structure/unionfind.hpp"
 #include "templates/alias.hpp"
 #include "templates/macro.hpp"
 
-int main() {}
+int main() {
+  ll N, M, K;
+  cin >> N >> M >> K;
+  map<ll, vpii> E;
+
+  rep(_, M) {
+    ll u, v, w;
+    cin >> u >> v >> w;
+
+    E[w].emplace_back(u - 1, v - 1);
+  }
+
+  UnionFind UF(N);
+  ll ans = 0;
+
+  vi cnt_a(N), cnt_b(N);
+
+  rep(_, K) {
+    ll a;
+    cin >> a;
+    cnt_a[a - 1]++;
+  }
+  rep(_, K) {
+    ll b;
+    cin >> b;
+    cnt_b[b - 1]++;
+  }
+
+  for (auto [w, l] : E) {
+    for (auto [u, v] : l) {
+      if (UF.same(u, v))
+        continue;
+
+      u = UF.leader(u);
+      v = UF.leader(v);
+
+      UF.merge(u, v);
+
+      if (UF.leader(u) == v)
+        swap(u, v);
+
+      cnt_a[u] += cnt_a[v];
+      cnt_b[u] += cnt_b[v];
+
+      ll mi = min(cnt_a[u], cnt_b[u]);
+      ans += mi * w;
+      cnt_a[u] -= mi;
+      cnt_b[u] -= mi;
+    }
+  }
+
+  cout << ans << "\n";
+}
