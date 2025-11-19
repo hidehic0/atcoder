045f00c4688f2389544d113e5b13ddd64bb81e49
hidehic0 <hidehico.ooba@gmail.com>
2025-11-19 11:41:26 +0900

    AC: ABC352-F

--- Training/ABC352/F/main.cpp
+++ Training/ABC352/F/main.cpp
@@ -1,50 +1,128 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+ll N, M;
+vvpii G;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+pair<ll, vi> get_rank(ll p, ll r) {
+  vi used(N, -1);
+  vi l = {p};
 
-void printbase() { cout << '\n'; }
+  ll res = 1 << r;
+  used[p] = r;
+  queue<ll> Q;
+  Q.emplace(p);
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  while (!Q.empty()) {
+    ll cur = Q.front();
+    Q.pop();
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+    for (auto [nxt, w] : G[cur]) {
+      if (used[nxt] == -1) {
+        if (used[cur] + w < 0 || used[cur] + w >= N)
+          return mp(-1, used);
+
+        l.emplace_back(nxt);
+        used[nxt] = used[cur] + w;
+        res |= 1 << used[nxt];
+        Q.emplace(nxt);
+      }
     }
-    cout << '\n';
+  }
+
+  return mp(res, used);
 }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+int main() {
+  cin >> N >> M;
+  G.resize(N);
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+  rep(_, M) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    a--;
+    b--;
+
+    G[a].emplace_back(b, -c);
+    G[b].emplace_back(a, c);
+  }
+
+  VC<VC<pair<ll, vi>>> L;
+  vb used(N, false);
+
+  rep(i, N) {
+    if (used[i])
+      continue;
+
+    L.emplace_back(VC<pair<ll, vi>>{});
+
+    rep(k, N) {
+      auto t = get_rank(i, k);
+
+      rep(p, N) {
+        if (t.second[p] != -1)
+          used[p] = true;
+      }
+
+      if (t.first != -1) {
+        L.back().emplace_back(t);
+      }
+    }
+  }
+
+  vvi ldp(L.size() + 1, vi(1 << N, 0)), rdp(L.size() + 1, vi(1 << N, 0));
+  ldp[0][0] = 1;
+  rdp.back()[0] = 1;
+
+  rrep(i, L.size()) {
+    rep(bit, 1 << N) {
+      rep(k, L[i].size()) {
+        if ((bit & L[i][k].first) == 0) {
+          rdp[i][bit | L[i][k].first] += rdp[i + 1][bit];
+        }
+      }
+    }
+  }
+
+  vi ans(N, -1);
+
+  rep(i, L.size()) {
+    vvi T;
+    ll su = 0;
+
+    rep(k, L[i].size()) {
+      ll cur = 0;
+
+      rep(bit, 1 << N) {
+        if ((bit & L[i][k].first) == 0) {
+          cur += rdp[i + 1][(1 << N) - (bit | L[i][k].first) - 1] * ldp[i][bit];
+        }
+      }
+
+      su += cur;
+      if (cur >= 1) {
+        T.emplace_back(L[i][k].second);
+      }
     }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+    if (T.size() == 1) {
+      rep(k, N) {
+        if (T.front()[k] != -1)
+          ans[k] = T.front()[k] + 1;
+      }
+    }
 
-int main()
-{
+    rep(bit, 1 << N) {
+      rep(k, L[i].size()) {
+        if ((bit & L[i][k].first) == 0) {
+          ldp[i + 1][bit | L[i][k].first] += ldp[i][bit];
+        }
+      }
+    }
+  }
+
+  out(ans);
 }
