c97ca0d0db824512c2574e109944e83e82003020
hidehic0 <hidehico.ooba@gmail.com>
2025-11-26 15:23:15 +0900

    AC: ABC356-F

--- Training/ABC356/f/main.cpp
+++ Training/ABC356/f/main.cpp
@@ -1,50 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/fenwicktree.hpp>
+#include <atcoder/segtree>
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
+#include "utils/compress.hpp"
 
-// マクロ
+ll Q, K;
+ll op(ll a, ll b) { return max(a, b); }
+ll e() { return -1e18; }
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+bool g(ll a) { return a <= K; }
 
-void printbase() { cout << '\n'; }
+int main() {
+  cin >> Q >> K;
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  vi T(Q), X(Q);
+  rep(i, Q) cin >> T[i] >> X[i];
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+  auto x_map = compress_1d(X);
+  ll N = x_map.size();
+
+  fenwick_tree<ll> ft(N);
+  segtree<ll, op, e> seg_l(N), seg_r(N);
+  si S;
+
+  rep(i, Q) {
+    if (T[i] == 1) {
+      if (S.count(X[i])) {
+        if (*S.begin() != X[i]) {
+          auto it = S.lower_bound(X[i]);
+          seg_r.set(
+              x_map[*prev(it)],
+              (it == prev(S.end()) ? LONG_LONG_MAX : *next(it) - *prev(it)));
+        }
+
+        if (*prev(S.end()) != X[i]) {
+          auto it = S.lower_bound(X[i]);
+          seg_l.set(x_map[*next(it)],
+                    (it == S.begin() ? LONG_LONG_MAX : *next(it) - *prev(it)));
+        }
+
+        seg_l.set(x_map[X[i]], e());
+        seg_r.set(x_map[X[i]], e());
+        ft.add(x_map[X[i]], -1);
+        S.erase(X[i]);
+        continue;
+      }
+
+      S.emplace(X[i]);
+      auto it = S.lower_bound(X[i]);
+
+      if (it != S.begin()) {
+        seg_r.set(x_map[*prev(it)], X[i] - *prev(it));
+        seg_l.set(x_map[X[i]], X[i] - *prev(it));
+      } else {
+        seg_l.set(x_map[X[i]], LONG_LONG_MAX);
+      }
+
+      if (it != prev(S.end())) {
+        seg_l.set(x_map[*next(it)], *next(it) - X[i]);
+        seg_r.set(x_map[X[i]], *next(it) - X[i]);
+      } else {
+        seg_r.set(x_map[X[i]], LONG_LONG_MAX);
+      }
+
+      ft.add(x_map[X[i]], 1);
+    } else {
+      ll res = 1, l, r;
+      l = seg_r.min_left<g>(x_map[X[i]]);
+      r = seg_l.max_right<g>(x_map[X[i]] + 1);
+
+      if (l != x_map[X[i]])
+        res += ft.sum(l, x_map[X[i]]);
+      if (r != x_map[X[i]])
+        res += ft.sum(x_map[X[i]] + 1, r);
+
+      cout << res << "\n";
     }
-    cout << '\n';
-}
-
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
-
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+  }
 }
