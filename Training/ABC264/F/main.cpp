8ec652e0413c3742ce641869249db8f83266f51e
hidehic0 <hidehico.ooba@gmail.com>
2025-12-02 15:52:51 +0900

    AC: ABC264-F

--- Training/ABC264/F/main.cpp
+++ Training/ABC264/F/main.cpp
@@ -1,50 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
-
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
-
-void printbase() { cout << '\n'; }
-
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
+ll f(ll color, ll r_rev, ll c_rev) {
+  return ((1 << 2) * color) + ((1 << 1) * r_rev) + c_rev;
 }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+int main() {
+  ll H, W;
+  cin >> H >> W;
+  vi R(H), C(W);
+  cin >> R >> C;
+
+  vvi A(H, vi(W));
+  rep(i, H) {
+    string S;
+    cin >> S;
+
+    rep(k, W) A[i][k] = S[k] - '0';
+  }
+
+  vvvi dp(H, vvi(W, vi(8, 1e18)));
+
+  if (A[0][0] == 0) {
+    dp[0][0][f(0, 0, 0)] = 0;
+    dp[0][0][f(0, 1, 1)] = R[0] + C[0];
+    dp[0][0][f(1, 1, 0)] = R[0];
+    dp[0][0][f(1, 0, 1)] = C[0];
+  } else {
+    dp[0][0][f(1, 0, 0)] = 0;
+    dp[0][0][f(1, 1, 1)] = R[0] + C[0];
+    dp[0][0][f(0, 1, 0)] = R[0];
+    dp[0][0][f(0, 0, 1)] = C[0];
+  }
+
+  rep(x, H) {
+    rep(y, W) {
+      rep(state, 8) {
+        ll color = (state >> 2) & 1, r_rev = (state >> 1) & 1,
+           c_rev = state & 1;
+
+        if (x + 1 < H) {
+          if (color != (A[x + 1][y] ^ c_rev)) {
+            chmin(dp[x + 1][y][f(color, 1, c_rev)], dp[x][y][state] + R[x + 1]);
+          } else {
+            chmin(dp[x + 1][y][f(color, 0, c_rev)], dp[x][y][state]);
+          }
+        }
+
+        if (y + 1 < W) {
+          if (color != (A[x][y + 1] ^ r_rev)) {
+            chmin(dp[x][y + 1][f(color, r_rev, 1)], dp[x][y][state] + C[y + 1]);
+          } else {
+            chmin(dp[x][y + 1][f(color, r_rev, 0)], dp[x][y][state]);
+          }
+        }
+      }
     }
-    cout << '\n';
-}
+  }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
-
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+  cout << *min_element(all(dp.back().back())) << "\n";
 }
