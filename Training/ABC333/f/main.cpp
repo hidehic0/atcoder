27353201d9f70ebe27f32d285a5e4bc34ea50535
hidehic0 <hidehico.ooba@gmail.com>
2025-12-12 10:10:43 +0900

    AC: ABC333-F

--- Training/ABC333/f/main.cpp
+++ Training/ABC333/f/main.cpp
@@ -1,50 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/modint>
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+using mint = modint998244353;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+int main() {
+  ll N;
+  cin >> N;
 
-void printbase() { cout << '\n'; }
+  VC<mint> P(N + 5, 1), PINV(N + 5, 1);
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  REP(i, 1, N + 4) {
+    P[i] = P[i - 1] * i;
+    PINV[i] = P[i].inv();
+  }
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
-    }
-    cout << '\n';
-}
+  VC<mint> dp(N + 2);
+  dp[0] = 1;
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+  REP(i, 1, N + 1) {
+    mint p = PINV[2].pow(i + 1) * ((1 - PINV[2].pow(i + 1)).inv());
+    REP(k, 1, i + 1) { dp[i] += P[i] * PINV[k] * PINV[i - k] * dp[i - k]; }
+    dp[i] *= p;
+  }
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
-    }
+  rep(i, N) {
+    mint res = 0;
+    rep(k, i + 1) { res += P[i] * PINV[k] * PINV[i - k] * dp[N - (i - k) - 1]; }
+    res /= mint{2}.pow(i);
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
-
-int main()
-{
+    cout << res.val() << (i + 1 == N ? "\n" : " ");
+  }
 }
