1ab052e0aadd12264cebdbaf1c6e24878364d676
hidehic0 <hidehico.ooba@gmail.com>
2025-12-12 13:50:44 +0900

    解説AC: ABC380-G
    順列の転倒数の和が分からなかった

--- Training/ABC380/g/main.cpp
+++ Training/ABC380/g/main.cpp
@@ -1,56 +1,67 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+using mint = modint998244353;
+using S = pair<mint, mint>;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+S op(S a, S b) { return {a.first + b.first, a.second + b.second}; }
+S e() { return {0, 0}; }
+S mapping(mint l, S r) { return {r.first + l * r.second, r.second}; }
+mint composition(mint l, mint r) { return l + r; }
+mint id() { return 0; }
 
-template <typename T> bool chmin(T &a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-template <typename T> bool chmax(T &a, T b) {
-  if (a < b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
+int main() {
+  ll N, K;
+  cin >> N >> K;
+  vi P(N);
+  cin >> P;
+  rep(i, N) P[i]--;
 
-void printbase() { cout << '\n'; }
+  lazy_segtree<S, op, e, mint, mapping, composition, id> seg(N);
+  fenwick_tree<mint> ft(N);
 
-template <typename T> void printbase(const T &t) { cout << t << '\n'; }
+  mint base = 0;
 
-template <typename T> void printbase(const std::vector<T> &vec) {
-  for (const auto &v : vec) {
-    cout << v << ' ';
-  }
-  cout << '\n';
-}
-
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail) {
-  cout << head << ' ';
-  printbase(tail...);
-}
-
-#define print(...)                                                             \
-  {                                                                            \
-    printbase(__VA_ARGS__);                                                    \
-    return 0;                                                                  \
+  rep(i, N) {
+    base += ft.sum(P[i], N);
+    ft.add(P[i], 1);
   }
 
-const ll INF = (ll)1 << 63;
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  VC<mint> F(2e5 + 100, 1), FINV(2e5 + 100, 1);
+  REP(i, 1, 2e5 + 100) {
+    F[i] = F[i - 1] * i;
+    FINV[i] = F[i].inv();
+  }
 
-int main() {}
+  mint nxt_inversions = F[K] * F[K] * FINV[2] * FINV[max(K - 2, 0LL)] * FINV[2];
+  if (K == 1)
+    nxt_inversions = 0;
+
+  // REP(i, 1, K) { nxt_inversions += mint{i} * mint{2}.pow(i - 1); }
+  mint ans = 0;
+  rrep(i, N) {
+    seg.set(P[i], {0, 1});
+
+    if (i + K < N) {
+      seg.set(P[i + K], e());
+    }
+
+    if (P[i] > 0)
+      seg.apply(0, P[i], 1);
+
+    if (i + K > N) {
+      continue;
+    }
+
+    ans += (base - seg.all_prod().first) * F[K] + nxt_inversions;
+  }
+
+  ans /= mint{N - K + 1} * F[K];
+
+  cout << ans.val() << "\n";
+}
