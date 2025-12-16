c2b2a72cdf763466b9c64c072de8ae9af3673e21
hidehic0 <hidehico.ooba@gmail.com>
2025-12-16 21:57:52 +0900

    AC: ABC232-F

--- Training/ABC232/f/main.cpp
+++ Training/ABC232/f/main.cpp
@@ -1,9 +1,37 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
-using namespace atcoder;
 
 #include "templates/alias.hpp"
 #include "templates/macro.hpp"
 
-int main() {}
+int main() {
+  ll N, X, Y;
+  cin >> N >> X >> Y;
+  vi A(N), B(N);
+  cin >> A >> B;
+
+  vi dp(1 << N, 5e18);
+  dp[0] = 0;
+
+  rep(bit, (1 << N) - 1) {
+    if (dp[bit] == 5e18)
+      continue;
+
+    ll M = popcount((unsigned int)bit);
+
+    rep(i, N) {
+      if ((bit >> i) & 1)
+        continue;
+
+      ll dis = 0;
+      rep(k, i) {
+        if (!((bit >> k) & 1))
+          dis++;
+      }
+
+      chmin(dp[bit | (1 << i)], dp[bit] + dis * Y + abs(A[i] - B[M]) * X);
+    }
+  }
+
+  cout << dp.back() << "\n";
+}
