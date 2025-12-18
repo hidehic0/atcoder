29282536c5555eb4932d087b950afba0183ba0bd
hidehic0 <hidehico.ooba@gmail.com>
2025-12-18 20:44:00 +0900

    AC: ABC380-F

--- Training/ABC380/f/main.cpp
+++ Training/ABC380/f/main.cpp
@@ -1,56 +1,76 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+ll N, M, L;
+vi D;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+map<tuple<ll, ll, ll>, bool> memo;
 
-template <typename T> bool chmin(T &a, T b) {
-  if (a > b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
-template <typename T> bool chmax(T &a, T b) {
-  if (a < b) {
-    a = b;
-    return true;
-  }
-  return false;
-}
+bool rec(ll bita, ll bitb, ll turn) {
+  if (memo.contains(tie(bita, bitb, turn)))
+    return memo[tie(bita, bitb, turn)];
 
-void printbase() { cout << '\n'; }
-
-template <typename T> void printbase(const T &t) { cout << t << '\n'; }
-
-template <typename T> void printbase(const std::vector<T> &vec) {
-  for (const auto &v : vec) {
-    cout << v << ' ';
-  }
-  cout << '\n';
-}
-
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail) {
-  cout << head << ' ';
-  printbase(tail...);
-}
-
-#define print(...)                                                             \
-  {                                                                            \
-    printbase(__VA_ARGS__);                                                    \
-    return 0;                                                                  \
+  vi A, B, C;
+  rep(i, D.size()) {
+    if ((bita >> i) & 1)
+      A.emplace_back(i);
+    else if ((bitb >> i) & 1)
+      B.emplace_back(i);
+    else
+      C.emplace_back(i);
   }
 
-const ll INF = (ll)1 << 63;
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  if (turn == 0) {
+    for (auto i : A) {
+      for (auto k : C) {
+        if (D[i] <= D[k])
+          continue;
 
-int main() {}
+        if (!rec((bita ^ (1 << i)) | (1 << k), bitb, turn ^ 1))
+          return memo[tie(bita, bitb, turn)] = true;
+      }
+
+      if (!rec(bita ^ (1 << i), bitb, turn ^ 1))
+        return memo[tie(bita, bitb, turn)] = true;
+    }
+  } else {
+    for (auto i : B) {
+      for (auto k : C) {
+        if (D[i] <= D[k])
+          continue;
+
+        if (!rec(bita, (bitb ^ (1 << i)) | (1 << k), turn ^ 1))
+          return memo[tie(bita, bitb, turn)] = true;
+      }
+
+      if (!rec(bita, bitb ^ (1 << i), turn ^ 1))
+        return memo[tie(bita, bitb, turn)] = true;
+    }
+  }
+
+  return memo[tie(bita, bitb, turn)] = false;
+}
+
+int main() {
+  cin >> N >> M >> L;
+  vi A(N), B(M), C(L);
+
+  cin >> A >> B >> C;
+  rep(i, N) D.emplace_back(A[i]);
+  rep(i, M) D.emplace_back(B[i]);
+  rep(i, L) D.emplace_back(C[i]);
+
+  ll bita = 0, bitb = 0;
+
+  rep(i, N) bita |= (1 << i);
+  REP(i, N, N + M) bitb |= (1 << i);
+
+  if (rec(bita, bitb, 0))
+    cout << "Takahashi";
+  else
+    cout << "Aoki";
+  cout << "\n";
+}
