6f8752287454231e2dd74319fbc22803b357b7c5
hidehic0 <hidehico.ooba@gmail.com>
2025-12-24 10:24:56 +0900

    AC: ABC361-E

--- Training/ABC359/e/main.cpp
+++ Training/ABC359/e/main.cpp
@@ -1,9 +1,31 @@
-#include <atcoder/all>
 #include <bits/stdc++.h>
 using namespace std;
-using namespace atcoder;
 
 #include "templates/alias.hpp"
 #include "templates/macro.hpp"
 
-int main() {}
+int main() {
+  ll N;
+  cin >> N;
+  vi H(N);
+  cin >> H;
+
+  stack<pii> ST;
+  ST.emplace(0, 1);
+
+  ll cur = 0;
+  rep(i, N) {
+    ll ns = 0;
+    while (!ST.empty() && ST.top().first < H[i]) {
+      ns += ST.top().second;
+      cur += (H[i] - ST.top().first) * ST.top().second;
+      ST.pop();
+    }
+
+    ST.emplace(H[i], ns);
+    ST.emplace(1, 1);
+    cur++;
+
+    cout << cur << (i + 1 == N ? "\n" : " ");
+  }
+}
