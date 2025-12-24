1925b9e91d939feb3e5f3075fe7de0032b8417f6
hidehic0 <hidehico.ooba@gmail.com>
2025-12-24 10:52:09 +0900

    AC: ABC361-E

--- Training/ABC361/e/main.cpp
+++ Training/ABC361/e/main.cpp
@@ -1,115 +1,41 @@
-#pragma GCC TARGET("O3")
-
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 
-// エラトステネスの篩
-vector<int> eratosthenes(int n) {
-  vector<bool> is_prime(n + 1, true);
-  is_prime[0] = is_prime[1] = false;
-  for (int i = 2; i * 1LL * i <= n; i++) {
-    if (is_prime[i]) {
-      for (int j = i * 2; j <= n; j += i) {
-        is_prime[j] = false;
-      }
-    }
-  }
-  vector<int> primes;
-  for (int i = 2; i <= n; i++) {
-    if (is_prime[i])
-      primes.push_back(i);
-  }
-  return primes;
-}
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// 複数数の素因数分解
-vector<vector<pair<int, int>>> factorization_plural(int nmax) {
-  vector<vector<pair<int, int>>> res(nmax + 1);
-  vector<int> primes = eratosthenes((int)sqrt(nmax) + 20);
+ll N;
+vvpii G;
 
-  for (int n = 1; n <= nmax; n++) {
-    int tmp = n;
-    for (int p : primes) {
-      if ((ll)p * p > tmp)
-        break;
-      if (tmp % p == 0) {
-        int cnt = 0;
-        while (tmp % p == 0) {
-          tmp /= p;
-          cnt++;
-        }
-        res[n].push_back({p, cnt});
-      }
-    }
-    if (tmp != 1) {
-      res[n].push_back({tmp, 1});
-    }
+pii dfs(ll cur, ll par = -1) {
+  pii res = mp(0, cur);
+  for (auto [nxt, w] : G[cur]) {
+    if (nxt == par)
+      continue;
+    pii c = dfs(nxt, cur);
+    chmax(res, mp(c.first + w, c.second));
   }
+
   return res;
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-
-  ll N;
   cin >> N;
+  G.resize(N);
 
-  ll ans = (ll)sqrtl(N);
+  ll ans = 0;
 
-  vector<int> L = eratosthenes(60);
-  unordered_set<int> prime_set(L.begin(), L.end());
-
-  int LIM = 1'000'001;
-  auto FL = factorization_plural(LIM);
-
-  for (int b = 3; b <= 61; b++) {
-    if (!prime_set.count(b))
-      continue;
-    if ((1LL << b) > N)
-      continue;
-
-    ll i = 2;
-    while (true) {
-      __int128 pow = 1;
-      for (int k = 0; k < b; k++) {
-        pow *= i;
-        if (pow > N)
-          break;
-      }
-      if (pow > N)
-        break;
-
-      // 素因数分解を取り出し
-      auto l = FL[i];
-      vector<pair<int, int>> factors;
-      for (auto [p, c] : l) {
-        factors.push_back({p, c * b});
-      }
-
-      bool ok = false;
-      for (int d = 2; d < b; d++) {
-        bool flag = true;
-        for (auto [p, c] : factors) {
-          if (c % d != 0) {
-            flag = false;
-            break;
-          }
-        }
-        if (flag) {
-          ok = true;
-          break;
-        }
-      }
-
-      if (!ok)
-        ans++;
-
-      i++;
-    }
+  rep(_, N - 1) {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    a--;
+    b--;
+    G[a].emplace_back(b, c);
+    G[b].emplace_back(a, c);
+    ans += c * 2;
   }
 
+  ans -= dfs(dfs(0).second).first;
+
   cout << ans << "\n";
-  return 0;
 }
