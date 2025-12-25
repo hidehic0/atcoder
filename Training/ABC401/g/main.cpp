3de51bf31b167815ca81b8581eeb8ba23a83da37
hidehic0 <hidehico.ooba@gmail.com>
2025-12-25 10:05:16 +0900

    解説AC: ABC401-G
    フローパートは分かったけど少数部分が分からなかった

--- Training/ABC401/g/main.cpp
+++ Training/ABC401/g/main.cpp
@@ -1,50 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/maxflow>
+using namespace atcoder;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+using T = tuple<ld, ll, ll>;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+int main() {
+  ll N;
+  cin >> N;
 
-void printbase() { cout << '\n'; }
+  VC<pair<ld, ld>> S(N), G(N);
+  cin >> S >> G;
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  VC<T> edges;
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+  rep(i, N) {
+    rep(k, N) {
+      ld dx = S[i].first - G[k].first, dy = S[i].second - G[k].second;
+      edges.emplace_back(sqrt(dx * dx + dy * dy), i, k);
     }
-    cout << '\n';
-}
+  }
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
+  sort(all(edges));
 
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+  ll left = -1, right = N * N;
+
+  while (right - left > 1) {
+    ll mid = (right + left) / 2;
+
+    mf_graph<ll> G(N * 2 + 2);
+
+    rep(i, mid + 1) {
+      auto [_, x, y] = edges[i];
+      G.add_edge(x, y + N, 1);
     }
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+    rep(i, N) {
+      G.add_edge(N * 2, i, 1);
+      G.add_edge(i + N, N * 2 + 1, 1);
+    }
 
-int main()
-{
+    if (G.flow(N * 2, N * 2 + 1) == N)
+      right = mid;
+    else
+      left = mid;
+  }
+
+  cout << fixed << setprecision(10) << get<0>(edges[right]) << "\n";
 }
