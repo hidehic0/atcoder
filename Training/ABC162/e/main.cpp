ff2e1f19172ed717fc054ab4e608b6877a89646a
hidehic0 <hidehico.ooba@gmail.com>
2026-01-05 13:23:46 +0900

    AC: ABC162-E

--- Training/ABC162/e/main.cpp
+++ Training/ABC162/e/main.cpp
@@ -1,9 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
+#include <atcoder/modint>
+using mint = atcoder::modint1000000007;
 
 #include "templates/alias.hpp"
 #include "templates/macro.hpp"
 
-int main() {}
+int main() {
+  ll N, K;
+  cin >> N >> K;
+
+  VC<mint> cnt(K + 1, 0);
+
+  RREP(k, 1, K + 1) {
+    cnt[k] = mint(K / k).pow(N);
+    ll i = k * 2;
+    while (i <= K) {
+      cnt[k] -= cnt[i];
+      i += k;
+    }
+  }
+
+  mint ans = 0;
+
+  REP(i, 1, K + 1) { ans += cnt[i] * i; }
+  cout << ans.val() << "\n";
+}
