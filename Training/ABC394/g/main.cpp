eb66d6e9df3400dffbf04878e1a9618fdaed1a70
hidehic0 <hidehico.ooba@gmail.com>
2026-01-07 10:10:06 +0900

    解説AC: ABC394-G

--- Training/ABC394/g/main.cpp
+++ Training/ABC394/g/main.cpp
@@ -1,50 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 型テンプレ
-using ll = long long;
-using ull = unsigned long long;
+#include "data-structure/unionfind.hpp"
+#include "grid/template.hpp"
+#include "templates/alias.hpp"
+#include "templates/macro.hpp"
 
-// マクロ
+int main() {
+  ll H, W;
+  cin >> H >> W;
+  vvi F(H, vi(W));
+  cin >> F;
+  map<ll, vpii> D;
 
-#define rep(i, n) for (ll i = 0; i < (int)(n); i++)
-#define all(a) (a).begin(), (a).end()
+  rep(i, H) rep(k, W) D[-F[i][k]].emplace_back(i, k);
 
-void printbase() { cout << '\n'; }
+  ll Q;
+  cin >> Q;
+  vpii L(Q);
+  VC<si> A(H * W);
 
-template <typename T>
-void printbase(const T &t)
-{
-    cout << t << '\n';
-}
+  vi ans(Q);
 
-template <typename T>
-void printbase(const std::vector<T> &vec)
-{
-    for (const auto &v : vec)
-    {
-        cout << v << ' ';
+  rep(i, Q) {
+    ll a, b, y, c, d, z;
+    cin >> a >> b >> y >> c >> d >> z;
+    if (mp(a, b) == mp(c, d))
+      ans[i] = abs(y - z);
+    else
+      A[(a - 1) * W + b - 1].emplace(i), A[(c - 1) * W + d - 1].emplace(i);
+
+    L[i] = mp(y, z);
+  }
+
+  UnionFind UF(H * W);
+
+  auto merge = [&](ll a, ll b, ll f) {
+    if (UF.same(a, b))
+      return;
+
+    a = UF.leader(a), b = UF.leader(b);
+    if (UF.data[a] > UF.data[b])
+      swap(a, b);
+
+    if (A[a].size() < A[b].size()) {
+      swap(A[a], A[b]);
     }
-    cout << '\n';
-}
 
-template <typename Head, typename... Tail>
-void printbase(const Head &head, const Tail &...tail)
-{
-    cout << head << ' ';
-    printbase(tail...);
-}
-
-#define print(...)              \
-    {                           \
-        printbase(__VA_ARGS__); \
-        return 0;               \
+    for (ll ind : A[b]) {
+      if (A[a].contains(ind)) {
+        if (max(L[ind].first, L[ind].second) <= f) {
+          ans[ind] = abs(L[ind].first - L[ind].second);
+        } else {
+          ans[ind] = abs(L[ind].first - f) + abs(L[ind].second - f);
+        }
+      } else {
+        A[a].emplace(ind);
+      }
     }
+    A[b].clear();
+    UF.merge(a, b);
+  };
 
-const ll INF = pow(10, 18);
-const string upperlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const string lowerlist = "abcdefghijklmnopqrstuvwxyz";
+  for (auto [f, l] : D) {
+    for (auto [x, y] : l) {
+      for (auto [mx, my] : MOVES1) {
+        ll nx = x + mx, ny = y + my;
+        if (coordinate_check(nx, ny, H, W) && F[nx][ny] >= F[x][y]) {
+          merge(x * W + y, nx * W + ny, -f);
+        }
+      }
+    }
+  }
 
-int main()
-{
+  rep(i, Q) cout << ans[i] << "\n";
 }
