91c3d79fba1951eb31bf08bfe0a33699cde41705
hidehic0 <hidehico.ooba@gmail.com>
2026-04-27 08:41:10 +0900

    update: upgrade pahcer

--- nix/pahcer/default.nix
+++ nix/pahcer/default.nix
@@ -1,23 +1,28 @@
-{lib,rustPlatform,fetchFromGitHub,...}:
-let 
-version = "0.3.1";
+{
+  lib,
+  rustPlatform,
+  fetchFromGitHub,
+  ...
+}:
+let
+  version = "0.4.0";
 in
-  rustPlatform.buildRustPackage {
-    pname = "pahcer";
-    version = "v${version}";
-    src = fetchFromGitHub{
-      owner = "terry-u16";
-      repo= "pahcer";
-      tag = "v${version}";
-      hash = "sha256-LNQsmAvoAbWipdI9MZfeZfQ8826Dz0EzwjvHiICfFec";
-    };
-    cargoHash="sha256-kcDBrcEkx+ouxB9wQZsQ7bGplaYQ56c7LS186DRCXDs";
-    meta =  {
-      description= "A tool to run tests for AtCoder Heuristic Contest (AHC)";
-      homepage = "https://github.com/terry-u16/pahcer";
-      license = with lib.licenses; [
-        mit
-        asl20
-      ];
-    };
-  }
+rustPlatform.buildRustPackage {
+  pname = "pahcer";
+  version = "v${version}";
+  src = fetchFromGitHub {
+    owner = "terry-u16";
+    repo = "pahcer";
+    tag = "v${version}";
+    hash = "sha256-y+BPwiqCSRavk7QtatDGzEtyDbfvx8fZ97ABtLWoyv0=";
+  };
+  cargoHash = "sha256-cuGm0LbxXfnyGUCWREVwg6PznaIIXgKaH0uxKYjp3XI=";
+  meta = {
+    description = "A tool to run tests for AtCoder Heuristic Contest (AHC)";
+    homepage = "https://github.com/terry-u16/pahcer";
+    license = with lib.licenses; [
+      mit
+      asl20
+    ];
+  };
+}
