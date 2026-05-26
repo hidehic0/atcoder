3a1f0f9a429037aa9c710275c0a1a271db19d8dc
hidehic0 <hidehico.ooba@gmail.com>
2026-05-26 09:17:23 +0900

    ABC459に参加した

---
+++ Performance/ABC459/f/main.cpp
@@ -0,0 +1,15 @@
+/**
+ library: https://github.com/hidehic0/library_cpp
+**/
+#include "all.h"
+
+using namespace std;
+using namespace atcoder;
+
+inline ll op(ll a, ll b) { return min(a, b); }
+inline ll e() { return 1e18; }
+inline ll mapping(ll a, ll b) { return a + b; }
+inline ll composition(ll a, ll b) { return a + b; }
+inline ll id() { return 0; }
+
+int main() {}
