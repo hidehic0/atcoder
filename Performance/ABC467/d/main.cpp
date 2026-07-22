a77a9a7451d0c07b8df17d740a4d0b1faf23f1d9
hidehic0 <hidehico.ooba@gmail.com>
2026-07-22 10:27:33 +0900

    ABC467とABC225に参加した

---
+++ Performance/ABC467/d/main.cpp
@@ -0,0 +1,9 @@
+/**
+ library: https://github.com/hidehic0/library_cpp
+**/
+#include "all.h"
+
+using namespace std;
+using namespace atcoder;
+
+int main() {}
---
+++ Performance/ARC225/c/main.cpp
@@ -0,0 +1,22 @@
+/**
+ library: https://github.com/hidehic0/library_cpp
+**/
+#include "all.h"
+
+using namespace std;
+using namespace atcoder;
+
+#include "tree/heavy_light_decomposition.hpp"
+
+void solve() {
+  ll N, M, K;
+  in(N, M, K);
+}
+
+int main() {
+  int T;
+  in(T);
+
+  while (T--)
+    solve();
+}
